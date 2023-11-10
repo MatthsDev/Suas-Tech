37840a5e107a5a7011abbf09042de4424fea78f6
Diego moreira <diegomoreira.pab@gmail.com>
2023-11-10 10:36:53 -0300

    dec

--- views/declaracao_prefeitura.php
+++ views/declaracao_prefeitura.php
@@ -1,23 +1,6 @@
 <?php
-include "../config/conexao.php";
-session_start(); // Inicie a sessão para acessar as variáveis de sessão
+include "../config/sessao.php";
 
-// Verifique o nível do usuário
-if (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] === 'admin') {
-    // O usuário é um administrador.
-    $voltar_link = '../painel-adm/adm-view.php';
-} elseif (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] === 'usuario') {
-    // O usuário é um usuário comum.
-    $voltar_link = '../painel-usuario/user-painel.php';
-} else {
-    // Redirecionar para a página de login ou exibir uma mensagem de erro, pois o nível do usuário não está definido.
-    $voltar_link = '../index.php'; // Altere o link para a página de login
-}
-// Verifica se o usuário está autenticado como admin ou usuário
-if (!isset($_SESSION['nome_usuario']) || ($_SESSION['nivel_usuario'] != 'admin' && $_SESSION['nivel_usuario'] != 'usuario')) {
-  header("location:../index.php");
-  exit; // Encerra o script após redirecionar
-}
 ?>
 
 <!DOCTYPE html>
@@ -32,22 +15,25 @@ if (!isset($_SESSION['nome_usuario']) || ($_SESSION['nivel_usuario'] != 'admin' 
     <title>Declaração Cadastro único</title>
 </head>
 
-<body>
-<div class="img">
+<body>    
+    <div class="img">
         <h1 class="titulo-com-imagem">
             <img src="../img/h1-declaração.svg" alt="Titulocomimagem">
         </h1>
     </div>
+<div class="container">
     <form method="post" action="../controller/declaracao/conferir.php">
         <select name="buscar_dados" required>
             <option value="cpf_dec">CPF:</option>
             <option value="nis_dec">NIS:</option>
         </select>
         <input type="text" name="valorescolhido" placeholder="Digite aqui:" required>
-        <br><br><br>
         <button type="submit">BUSCAR</button>
-        <div class="linha"></div> 
     </form>
+    <div class=lin1>    
+        <div class="linha"></div> 
+    </div>    
+</div>    
 </body>
 
 </html>
