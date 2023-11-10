633797280020a62e443ec894324ba07899369faa
Diego moreira <diegomoreira.pab@gmail.com>
2023-11-10 13:06:32 -0300

    123

--- views/visitas.php
+++ views/visitas.php
@@ -1,6 +1,24 @@
-<?php 
-require_once ('../config/sessao.php'); ?>
+<?php
+include "../config/conexao.php";
+session_start(); // Inicie a sessão para acessar as variáveis de sessão
 
+// Verifique o nível do usuário
+if (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] === 'admin') {
+    // O usuário é um administrador.
+    $voltar_link = '../painel-adm/adm-view.php';
+} elseif (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] === 'usuario') {
+    // O usuário é um usuário comum.
+    $voltar_link = '../painel-usuario/user-painel.php';
+} else {
+    // Redirecionar para a página de login ou exibir uma mensagem de erro, pois o nível do usuário não está definido.
+    $voltar_link = '../index.php'; // Altere o link para a página de login
+}
+// Verifica se o usuário está autenticado como admin ou usuário
+if (!isset($_SESSION['nome_usuario']) || ($_SESSION['nivel_usuario'] != 'admin' && $_SESSION['nivel_usuario'] != 'usuario')) {
+  header("location:../index.php");
+  exit; // Encerra o script após redirecionar
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
   <head>
