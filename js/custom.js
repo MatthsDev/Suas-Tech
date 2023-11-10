9c1d0e9e2035f028239aafeffa3f647a760af485
MatthsDev <137323572+MatthsDev@users.noreply.github.com>
2023-11-10 11:41:32 -0300

    atend

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,5 @@
 async function gerarSenha(tipoSenha){
     const dados = await fetch('gerar_senha.php?tipo=' + tipoSenha)
+    const resposta = await dados.json();
+    console.log(resposta)
 }
--- views/gerar_senha.php
+++ views/gerar_senha.php
@@ -3,7 +3,9 @@
 $tipo = filter_input(INPUT_GET, 'tipo', FILTER_SANITIZE_NUMBER_INT);
 
 if(!empty($tipo)){
-
+    $retorna = ['status' => false, 'mss' => "<p style='color: green;'> Senha gerada!</p>"];
 }else{
-    // $retorna = ['status' => true, 'mss']
+    $retorna = ['status' => true, 'mss' => "<p style='color: #f00;'> Erro: Senha não gerada!</p>"];
 }
+
+echo json_encode($retorna);
