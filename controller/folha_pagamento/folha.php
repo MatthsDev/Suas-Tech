42176dfd51d7df88837aa8a7b24f0789891fa66f
emmanuelcadete <engmecanicocadete@gmail.com>
2023-11-10 10:52:51 -0300

    Update folha.php
    estrutura organizada

--- controller/folha_pagamento/folha.php
+++ controller/folha_pagamento/folha.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start(); // Inicie a sessão para acessar as variáveis de sessão
 
 // Verifique o nível do usuário
@@ -12,7 +12,7 @@ if (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] === 'admin')
     // Redirecionar para a página de login ou exibir uma mensagem de erro, pois o nível do usuário não está definido.
     $voltar_link = '../index.php'; // Altere o link para a página de login
 }
-require_once ('../../config/sessao.php');
+require_once '../../config/sessao.php';
 ini_set('memory_limit', '256M');
 ?>
 <!DOCTYPE html>
@@ -32,53 +32,53 @@ ini_set('memory_limit', '256M');
             <img src="h1.svg" alt="Titulocomimagem">
         </h1>
     </div>
-    <div class="container">    
-        <div class="busca">    
+    <div class="container">
+        <div class="busca">
             <form action="">
                 <input name="cod_fam" class="busca2" placeholder="Digite o CPF ou NOME do beneficiário." type="text" required>
                 <button type="submit">Buscar</button>
             <a
-             href="<?php echo $voltar_link; ?>">
+            href="<?php echo $voltar_link; ?>">
                 <i class="fas fa-arrow-left"></i> Voltar ao menu
             </a>
             </form>
-        </div>    
+        </div>
         <table width="650px" border="1">
-        
+
             <tr class="titulo" >
-                
-                <th class="cabecalho">CÓDIGO FAMILIAR</th>    
+
+                <th class="cabecalho">CÓDIGO FAMILIAR</th>
                 <th class="cabecalho">NOME</th>
                 <th class="cabecalho">NIS</th>
                 <th class="cabecalho">BENEFÍCIO</th>
                 <th class="cabecalho">DATA ATUALIZAÇÃO</th>
                 <th class="cabecalho">TIPO BENEFÍCIO</th>
-                    
-            </tr>
-        
-                <?php
-            if (!isset($_GET['cod_fam'])){
-            ?>
 
-        <tr>    
-            <td colspan="5">Resultados da pesquisa</td>   
+            </tr>
+
+                <?php
+if (!isset($_GET['cod_fam'])) {
+    ?>
+
+        <tr>
+            <td colspan="5">Resultados da pesquisa</td>
         </tr>
 
         <?php
-            }else{
-                $sql_cod = $conn->real_escape_string($_GET['cod_fam']);
-                $sql_dados = "SELECT * FROM folha_pag WHERE rf_cpf LIKE '%$sql_cod%' OR rf_nome LIKE '%$sql_cod%' ";
-                $sql_query = $conn->query($sql_dados) or die("ERRO ao consultar !" . $conn-error);
+} else {
+    $sql_cod = $conn->real_escape_string($_GET['cod_fam']);
+    $sql_dados = "SELECT * FROM folha_pag WHERE rf_cpf LIKE '%$sql_cod%' OR rf_nome LIKE '%$sql_cod%' ";
+    $sql_query = $conn->query($sql_dados) or die("ERRO ao consultar !" . $conn - error);
 
-                if($sql_query->num_rows == 0){
-            ?>
+    if ($sql_query->num_rows == 0) {
+        ?>
             <tr class="resultado">
         <td colspan="5">Nenhum resultado encontrado...</td>
             </tr>
                     <?php
-                } else {
-                while ($dados = $sql_query->fetch_assoc()){
-                    ?>
+} else {
+        while ($dados = $sql_query->fetch_assoc()) {
+            ?>
                     <tr class="resultado">
                         <td class="resultado"><?php echo $dados['cod_familiar']; ?></td>
                         <td class="resultado"><?php echo $dados['rf_nome']; ?></td>
@@ -88,9 +88,9 @@ ini_set('memory_limit', '256M');
                         <td class="resultado"><?php echo $dados['tp_benef']; ?></td>
                     </tr>
     <?php
-                }
-            }
-        } ?>    
+}
+    }
+}?>
         </table>
     </div>
 </body>
