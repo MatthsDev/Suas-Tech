c2aded4ca053dddb6d03662a344779d0905392c3
Diego moreira <diegomoreira.pab@gmail.com>
2023-11-10 13:37:39 -0300

    dec

--- views/declaracao_prefeitura.php
+++ views/declaracao_prefeitura.php
@@ -1,5 +1,18 @@
-<?php
-include "../config/sessao.php";
+<?php 
+session_start(); // Inicie a sessão para acessar as variáveis de sessão
+include('../config/conexao.php');
+
+// Verifique o nível do usuário
+if (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] === 'admin') {
+    // O usuário é um administrador.
+    $voltar_link = '../painel-adm/adm-view.php';
+} elseif (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] === 'usuario') {
+    // O usuário é um usuário comum.
+    $voltar_link = '../painel-usuario/user-painel.php';
+} else {
+    // Redirecionar para a página de login ou exibir uma mensagem de erro, pois o nível do usuário não está definido.
+    $voltar_link = '../index.php'; // Altere o link para a página de login
+}
 
 ?>
 
@@ -12,6 +25,7 @@ include "../config/sessao.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../css/styledec.css">
     <link rel="website icon" type="png" href="../img/logo.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <title>Declaração Cadastro único</title>
 </head>
 
@@ -29,6 +43,10 @@ include "../config/sessao.php";
         </select>
         <input type="text" name="valorescolhido" placeholder="Digite aqui:" required>
         <button type="submit">BUSCAR</button>
+        <a
+            href="<?php echo $voltar_link; ?>">
+                <i class="fas fa-arrow-left"></i> Voltar ao menu
+            </a>
     </form>
     <div class=lin1>    
         <div class="linha"></div> 
