125e865215c5f5efaf9a71d40f2bfe77b14f7566
emmanuelcadete <engmecanicocadete@gmail.com>
2023-11-10 10:59:20 -0300

    Update folha.php
    meta acrescentado

--- controller/folha_pagamento/folha.php
+++ controller/folha_pagamento/folha.php
@@ -20,6 +20,7 @@ ini_set('memory_limit', '256M');
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Folha de Pagamento</title>
     <link rel="stylesheet" type="text/css" href="style-folha.css">
