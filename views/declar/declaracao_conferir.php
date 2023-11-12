128b546221488b5bdc7047e7a478bbaa4441e6aa
Diego moreira <diegomoreira.pab@gmail.com>
2023-11-12 18:46:24 -0300

    1

--- views/declar/declaracao_conferir.php
+++ views/declar/declaracao_conferir.php
@@ -9,7 +9,7 @@ require_once '../../controller/validar_cpf.php';
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="../../style-registrar.css">
+        <link rel="stylesheet" type="text/css" href="../../css/.style-registrar.css">
         <link rel="website icon" type="png" href="../../img/logo.png">
         <title>Declaração</title>
     </head>
