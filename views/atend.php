aa9496b905d925591e85824203f690603a536c52
emmanuelcadete <engmecanicocadete@gmail.com>
2023-11-11 23:33:39 -0300

    Update atend.php

--- views/atend.php
+++ views/atend.php
@@ -1,24 +1,11 @@
-<!-- view/index.php -->
-
 <!DOCTYPE html>
-<html lang="pt-br">
-
+<html lang="pt-BR">
 <head>
-    <meta charset="UTF-8" />
-    <title>Seu Projeto</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
-
 <body>
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <script src="../js/custom.js"></script>
-    <h2>Gerar senha de atendimento</h2>
-
-    <span id="msgAlerta"></span>
-    <p>Senha: <span id="senhaGerada"></span></p>
     
-    <p><button onclick="gerarSenha(1)">Gerar Senha Tipo 1</button></p>
-    <p><button onclick="gerarSenha(2)">Gerar Senha Tipo 2</button></p>
-    <!-- Adicione botões ou elementos conforme necessário -->
 </body>
-
 </html>
