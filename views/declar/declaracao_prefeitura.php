3e8afba61bdfa5e60afec5d72a7e3d8006fb2115
Diego moreira <diegomoreira.pab@gmail.com>
2023-11-12 18:47:02 -0300

    1

--- views/declar/declaracao_prefeitura.php
+++ views/declar/declaracao_prefeitura.php
@@ -23,7 +23,7 @@ if (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] === 'admin')
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../../css/styledec.css">
-    <link rel="website icon" type="png" href="../../img/icon.png">
+    <link rel="website icon" type="png" href="../../img/logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <title>Declaração Cadastro único</title>
 </head>
