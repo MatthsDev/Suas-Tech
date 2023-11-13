e8a43d17a93bfb10f941c7580f276357baa85d58
emmanuelcadete <engmecanicocadete@gmail.com>
2023-11-13 16:00:55 -0300

    sdfg

--- concessao/index.php
+++ concessao/index.php
@@ -1,2 +1,16 @@
 <?php
 
+session_start(); // Inicie a sessão para acessar as variáveis de sessão
+include '../cadunico/config/conexao.php';
+
+// Verifique o nível do usuário
+if (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] === 'admin') {
+    // O usuário é um administrador.
+    $voltar_link = '../../painel-adm/adm-view.php';
+} elseif (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] === 'usuario') {
+    // O usuário é um usuário comum.
+    $voltar_link = '../../painel-usuario/user-painel.php';
+} else {
+    // Redirecionar para a página de login ou exibir uma mensagem de erro, pois o nível do usuário não está definido.
+    $voltar_link = '../../index.php'; // Altere o link para a página de login
+}
