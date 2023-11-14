1720d873a1541534612ec1652b5897910be48ec3
emmanuelcadete <engmecanicocadete@gmail.com>
2023-11-14 12:30:28 -0300

    caminho declaração

--- cadunico/views/declar/declaracao_prefeitura.php
+++ cadunico/views/declar/declaracao_prefeitura.php
@@ -1,6 +1,6 @@
 <?php
 session_start(); // Inicie a sessão para acessar as variáveis de sessão
-require_once '../../config/conexao.php';
+require_once '../../../config/conexao.php';
 
 // Verifique o nível do usuário
 if (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] === 'admin') {
