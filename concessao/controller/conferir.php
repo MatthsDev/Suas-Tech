756556cfefdb49924697802aebd6eadc56679629
emmanuelcadete <engmecanicocadete@gmail.com>
2023-11-14 08:42:55 -0300

    puxa valor do formulário

--- concessao/controller/conferir.php
+++ concessao/controller/conferir.php
@@ -1,3 +1,10 @@
 <?php
 
 require_once '../../cadunico/controller/validar_cpf.php';
+
+if (isset($_POST['buscar_dados']) && !empty($_POST['buscar_dados'])) {
+    $opcao = $_POST['buscar_dados'];
+    if($opcao == 'cpf_dec'){
+        
+    }
+}
