8260ead28dd0c870ca6a2e1bbdc7983fdfa36818
emmanuelcadete <engmecanicocadete@gmail.com>
2023-11-14 08:44:51 -0300

    comentários

--- concessao/controller/conferir.php
+++ concessao/controller/conferir.php
@@ -2,9 +2,10 @@
 
 require_once '../../cadunico/controller/validar_cpf.php';
 
+//puxa as informações do formulário '../index.php'
 if (isset($_POST['buscar_dados']) && !empty($_POST['buscar_dados'])) {
     $opcao = $_POST['buscar_dados'];
     if($opcao == 'cpf_dec'){
-        
+    //cria as variáveis com o valor recebido
     }
 }
