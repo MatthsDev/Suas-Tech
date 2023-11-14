727509469387fbaace5163e7698ba3e0498b04eb
emmanuelcadete <engmecanicocadete@gmail.com>
2023-11-14 11:39:28 -0300

    Pasta cofing e controller estão fora do cadunico

--- concessao/index.php
+++ concessao/index.php
@@ -6,13 +6,13 @@ include '../cadunico/config/conexao.php';
 // Verifique o nível do usuário
 if (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] === 'admin') {
     // O usuário é um administrador.
-    $voltar_link = '../../painel-adm/adm-view.php';
+    $voltar_link = 'painel-adm/adm-view.php';
 } elseif (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] === 'usuario') {
     // O usuário é um usuário comum.
-    $voltar_link = '../../painel-usuario/user-painel.php';
+    $voltar_link = 'painel-usuario/user-painel.php';
 } else {
     // Redirecionar para a página de login ou exibir uma mensagem de erro, pois o nível do usuário não está definido.
-    $voltar_link = '../../index.php'; // Altere o link para a página de login
+    $voltar_link = '../index.php'; // Altere o link para a página de login
 }
 ?>
 <!DOCTYPE html>
@@ -45,9 +45,5 @@ if (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] === 'admin')
         <div class="linha"></div>
     </div>
 </div>
-<a
-            href="<?php echo $voltar_link; ?>">
-                <i class="fas fa-arrow-left"></i> Voltar ao menu
-            </a>
 </body>
 </html>
