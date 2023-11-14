5ebeefe141c7f46130501e5b3816bc35bf7a1507
emmanuelcadete <engmecanicocadete@gmail.com>
2023-11-14 16:57:53 -0300

    123

--- cadunico/painel-adm/cadastro_user.php
+++ cadunico/painel-adm/cadastro_user.php
@@ -21,15 +21,16 @@ include_once '../../config/sessao.php';
     <h1>Cadastro de Usuários</h1>
 
     <div class="container">
-    <form action="post">
+    <form method="post" action="post">
         <label>CPF: </label>
         <input type="text" name="cpf_dec" placeholder="Digite o CPF..." required><br>
-        
+
         <label>NOME: </label>
         <input type="text" name="nome_dec" placeholder="Digite o nome completo..." required><br>
 
         <label>Tipo de acesso: </label>
         <select name="buscar_dados" required>
+            <option value="" disabled selected hidden>Selecione o perfil</option>
             <option value="adm">Administrador</option>
             <option value="usuario">Usuário</option>
         </select>
@@ -41,16 +42,20 @@ include_once '../../config/sessao.php';
             </a>
     </form>
     <?php
-    require_once '../../config/validar_cpf.php';
-        if (!isset($_POST['cpf_dec'])){
+require_once '../../config/validar_cpf.php';
+if (!isset($_POST['cpf_dec'])) {
 
-        }else{
-            $cpf_dec = $_POST['cpf_dec'];
-            //$smtp = $conn->prepare("INSERT INTO usuarios (cpf_dec, nome_dec, buscar_dados, setor) VALUES (?,?,?,?)");
-            //$smtp->bind_param("sssss", $codigo_familiar, $nomerf, $data_visita, $acao_visita, $parecer);
-            echo $cpf_dec;
-        }
-    ?>
+} else {
+    $cpf_dec = $_POST['cpf_dec'];
+    $nome_dec = $_POST['nome_dec'];
+    $tpacesso = $_POST['buscar_dados'];
+    //$smtp = $conn->prepare("INSERT INTO usuarios (cpf_dec, nome_dec, buscar_dados, setor) VALUES (?,?,?,?)");
+    //$smtp->bind_param("sssss", $codigo_familiar, $nomerf, $data_visita, $acao_visita, $parecer);
+    echo $nome_dec;
+    echo $cpf_dec;
+    echo $tpacesso;
+}
+?>
     <div class=lin1>
         <div class="linha"></div>
     </div>
