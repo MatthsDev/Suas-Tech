03947f773335c71495e64ad8d2141dbff0f9d3b0
Diego-m-dev <diegomoreira.pab@gmail.com>
2023-11-14 14:45:55 -0300

    2

--- cadunico/controller/folha_pagamento/folha.php
+++ cadunico/controller/folha_pagamento/folha.php
@@ -1,18 +1,6 @@
 <?php 
-session_start(); // Inicie a sessão para acessar as variáveis de sessão
-
-// Verifique o nível do usuário
-if (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] === 'admin') {
-    // O usuário é um administrador.
-    $voltar_link = '../../painel-adm/adm-view.php';
-} elseif (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] === 'usuario') {
-    // O usuário é um usuário comum.
-    $voltar_link = '../../painel-usuario/user-painel.php';
-} else {
-    // Redirecionar para a página de login ou exibir uma mensagem de erro, pois o nível do usuário não está definido.
-    $voltar_link = '../index.php'; // Altere o link para a página de login
-}
 include('../../../config/conexao.php');
+include('../../../config/sessao.php');
 ini_set('memory_limit', '256M');
 ?>
 <!DOCTYPE html>
