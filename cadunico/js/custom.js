bf764a02021a3e08e672b046dcc5885e2461ac41
MatthsDev <137323572+MatthsDev@users.noreply.github.com>
2023-11-14 17:25:41 -0300

    1

--- cadunico/js/custom.js
+++ cadunico/js/custom.js
@@ -1,16 +1,23 @@
 async function gerarSenha(tipoSenha) {
     try {
         const dados = await fetch('../controller/atendimento/gerar_senha.php?tipo=' + tipoSenha);
+        
+        if (!dados.ok) {
+            throw new Error('Erro na requisição: ' + dados.status);
+        }
+
         const resposta = await dados.json();
 
-        if (!resposta['status']) {
-            document.getElementById("msgAlerta").innerHTML = resposta['msg'];
+        if (!resposta.status) {
+            document.getElementById("msgAlerta").innerHTML = resposta.msg;
             document.getElementById("senhaGerada").innerHTML = "";
         } else {
-            document.getElementById("senhaGerada").innerHTML = resposta['nome_senha'];
+            document.getElementById("senhaGerada").innerHTML = resposta.nome_senha;
             document.getElementById("msgAlerta").innerHTML = "";
         }
     } catch (error) {
-        console.error('Erro:', error);
+        console.error('Erro:', error.message);
+        document.getElementById("msgAlerta").innerHTML = 'Erro na requisição. Verifique o console para mais detalhes.';
+        document.getElementById("senhaGerada").innerHTML = "";
     }
 }
--- cadunico/views/atendimento/atendimento.php
+++ cadunico/views/atendimento/atendimento.php
@@ -1,6 +1,7 @@
 <?php
-include_once "./conexao.php";
+include_once "../../../config/conexao.php";
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -25,7 +26,7 @@ include_once "./conexao.php";
     // Botao para chamar a funcao "chamarSenha". Funcao que chama a senha  
     echo "<p><button type='button' onclick='chamarSenha(2)'>Preferencial</button></p>";
 
-    // Recuperar a senhas geradas que estao salva na tabela "senhas_geradas", com a situacao 2 "Emitida"
+    // Recuperar a senhas geradas que estao salvas na tabela "senhas_geradas", com a situacao 2 "Emitida"
     $query_senhas_geradas = "SELECT senger.id,
                     sen.nome_senha,
                     tip.nome
@@ -34,43 +35,114 @@ include_once "./conexao.php";
                     INNER JOIN tipos_senhas AS tip ON tip.id=sen.tipos_senha_id
                     WHERE senger.sits_senha_id = 2 
                     ORDER BY senger.id ASC";
-    //Prepara a QUERY
+
+    // Preparar a QUERY
     $result_senhas_geradas = $conn->prepare($query_senhas_geradas);
 
-    //Executar a QUERY
+    // Executar a QUERY
     $result_senhas_geradas->execute();
 
-    // Inicio SELETOR que esta ao redor da lista de senha que he utilizada no JS para excluir a senha chamada
+    // Inicio SELETOR que esta ao redor da lista de senha que é utilizada no JS para excluir a senha chamada
     echo "<div id='lista-senha-gerada'>";
 
-    // Ler os dados retornado do banco de dados
-    while ($row_senha_gerada = $result_senhas_geradas->fetch(PDO::FETCH_ASSOC)){
-        //var_dump($row_senha_gerada);
-
+    // Ler os dados retornados do banco de dados
+    while ($row_senha_gerada = $result_senhas_geradas->fetch(PDO::FETCH_ASSOC)) {
         // Extrair para imprimir através da chave no array
         extract($row_senha_gerada);
 
-        // Inicio SELETOR utilizado para indicar qual senha deve ser excluida quando a mesma for chamada
+        // Inicio SELETOR utilizado para indicar qual senha deve ser excluída quando a mesma for chamada
         echo "<div id='senha-gerada-$id'>";
 
         // Imprimir os dados da senha
         echo "ID da senha gerada: $id <br>";
         echo "Nome da senha: $nome_senha <br>";
-        echo "Tipo da senha: $nome <br>";        
+        echo "Tipo da senha: $nome <br>";
 
         echo "<hr>";
 
-        // Fim SELETOR utilizado para indicar qual senha deve ser excluida quando a mesma for chamada
+        // Fim SELETOR utilizado para indicar qual senha deve ser excluída quando a mesma for chamada
         echo "</div>";
-        
     }
 
-    // Fim SELETOR que esta ao redor da lista de senha que he utilizada no JS para excluir a senha chamada
+    // Fim SELETOR que esta ao redor da lista de senha que é utilizada no JS para excluir a senha chamada
     echo "</div>";
     ?>
 
     <script src="js/custom.js"></script>
+</body>
 
+</html>
+<?php
+include_once "../../../config/conexao.php";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Celke</title>
+</head>
+
+<body>
+    <a href="index.php">Gerar Senha</a><br>
+    <a href="atendimento.php">Chamar Senha</a><br>
+
+    <h2>Chamar Senha</h2>
+
+    <!-- Receber a mensagem de erro do JavaScript -->
+    <span id="msgAlerta">Senha chamada: </span>
+
+    <?php
+    // Botao para chamar a funcao "chamarSenha". Funcao que chama a senha 
+    echo "<p><button type='button' onclick='chamarSenha(1)'>Convencional</button></p>";
+
+    // Botao para chamar a funcao "chamarSenha". Funcao que chama a senha  
+    echo "<p><button type='button' onclick='chamarSenha(2)'>Preferencial</button></p>";
+
+    // Recuperar a senhas geradas que estao salvas na tabela "senhas_geradas", com a situacao 2 "Emitida"
+    $query_senhas_geradas = "SELECT senger.id,
+                    sen.nome_senha,
+                    tip.nome
+                    FROM senhas_geradas AS senger
+                    INNER JOIN senhas AS sen ON sen.id=senger.senha_id
+                    INNER JOIN tipos_senhas AS tip ON tip.id=sen.tipos_senha_id
+                    WHERE senger.sits_senha_id = 2 
+                    ORDER BY senger.id ASC";
+
+    // Preparar a QUERY
+    $result_senhas_geradas = $conn->prepare($query_senhas_geradas);
+
+    // Executar a QUERY
+    $result_senhas_geradas->execute();
+
+    // Inicio SELETOR que esta ao redor da lista de senha que é utilizada no JS para excluir a senha chamada
+    echo "<div id='lista-senha-gerada'>";
+
+    // Ler os dados retornados do banco de dados
+    while ($row_senha_gerada = $result_senhas_geradas->fetch(PDO::FETCH_ASSOC)) {
+        // Extrair para imprimir através da chave no array
+        extract($row_senha_gerada);
+
+        // Inicio SELETOR utilizado para indicar qual senha deve ser excluída quando a mesma for chamada
+        echo "<div id='senha-gerada-$id'>";
+
+        // Imprimir os dados da senha
+        echo "ID da senha gerada: $id <br>";
+        echo "Nome da senha: $nome_senha <br>";
+        echo "Tipo da senha: $nome <br>";
+
+        echo "<hr>";
+
+        // Fim SELETOR utilizado para indicar qual senha deve ser excluída quando a mesma for chamada
+        echo "</div>";
+    }
+
+    // Fim SELETOR que esta ao redor da lista de senha que é utilizada no JS para excluir a senha chamada
+    echo "</div>";
+    ?>
+
+    <script src="js/custom.js"></script>
 </body>
 
 </html>
