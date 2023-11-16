337a795212a3f863e4d38e5392ce266fdb0b4a52
emmanuelcadete <engmecanicocadete@gmail.com>
2023-11-16 20:59:46 -0300

    acessando a primeira tela

--- config/autenticar.php
+++ config/autenticar.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 
-require_once("conexao.php");
+require_once "conexao.php";
 session_start();
 
-if(empty($_POST['usuario']) || empty($_POST['senha'])){
-	header("location:../index.php");
+if (empty($_POST['usuario']) || empty($_POST['senha'])) {
+    header("location:../index.php");
 }
 
 $usuario = $_POST['usuario'];
@@ -18,32 +18,37 @@ $res->execute();
 $dados = $res->fetchAll(PDO::FETCH_ASSOC);
 $linhas = count($dados);
 
-if($linhas > 0){
-	$_SESSION['nome_usuario'] = $dados[0]['nome'];
-	$_SESSION['user_usuario'] = $dados[0]['usuario'];
-	$_SESSION['nivel_usuario'] = $dados[0]['nivel'];
+if ($linhas > 0) {
+    $_SESSION['nome_usuario'] = $dados[0]['nome'];
+    $_SESSION['user_usuario'] = $dados[0]['usuario'];
+    $_SESSION['nivel_usuario'] = $dados[0]['nivel'];
 
-	if($_SESSION['nivel_usuario'] == 'admin'){
-		header("location:../cadunico/painel-adm/adm-view.php");
-		exit();
-	}
+    // Verifique se é o primeiro acesso pela senha
+    if ($senha == "@senha123") {
+        // Redirecione para a página de conclusão do cadastro
+        header("Location: ../cadunico/painel-usuario/primeiro_acesso.php");
+        exit();
+    }
 
-	if($_SESSION['nivel_usuario'] == 'usuario'){
-		header("location:../cadunico/painel-usuario/user-painel.php");
-		exit();
-	}
+    if ($_SESSION['nivel_usuario'] == 'admin') {
+        header("location:../cadunico/painel-adm/adm-view.php");
+        exit();
+    }
 
-}else{
-	echo "<script language='javascript'>window.alert('Dados Incorretos!!'); </script>";
-	echo "<script language='javascript'>window.location='../index.php'; </script>";
+    if ($_SESSION['nivel_usuario'] == 'usuario') {
+        header("location:../cadunico/painel-usuario/user-painel.php");
+        exit();
+    }
+
+} else {
+    echo "<script language='javascript'>window.alert('Dados Incorretos!!'); </script>";
+    echo "<script language='javascript'>window.location='../index.php'; </script>";
 }
-
 
 //FUNÇÃO BOTAO
 
-    if(isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] == 'admin'){
-        echo '<a href="../painel-adm/adm-view.php">Ir para o Painel de Admin</a>';
-    } elseif(isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] == 'usuario'){
-        echo '<a href="../painel-usuario/user-painel.php">Ir para o Painel de Usuário</a>';
-    }
-?>
+if (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] == 'admin') {
+    echo '<a href="../painel-adm/adm-view.php">Ir para o Painel de Admin</a>';
+} elseif (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] == 'usuario') {
+    echo '<a href="../painel-usuario/user-painel.php">Ir para o Painel de Usuário</a>';
+}
