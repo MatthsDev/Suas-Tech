67c1064bcf6f13c054eabd5569b9e8015f5511a2
Diego-m-dev <diegomoreira.pab@gmail.com>
2023-11-17 13:42:21 -0300

    1

--- cadunico/painel-usuario/primeiro_acesso.php
+++ cadunico/painel-usuario/primeiro_acesso.php
@@ -3,7 +3,7 @@ require_once '../../config/conexao.php';
 session_start();
 
 $nome_user = $_SESSION['nome_user_1_acesso'];
-echo $nome_user;
+$nome_user;
 $_SESSION['nome_user_1_acesso'] = $nome_user;
 ?>
 <!DOCTYPE html>
@@ -12,7 +12,7 @@ $_SESSION['nome_user_1_acesso'] = $nome_user;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TechSUAS - Menu</title>
-    <link rel="stylesheet" href="../css/adm.css">
+    <link rel="stylesheet" href="../css/p-acesso.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="website icon" type="png" href="../img/logo.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10">
@@ -23,42 +23,43 @@ $_SESSION['nome_user_1_acesso'] = $nome_user;
 <body>
     <div class="img">
     <h1 class="titulo-com-imagem">
-        <img class="titulo-com-imagem" src="../img/h1-primeiro.svg" alt="Titulocomimagem">
+        <img class="titulo-com-imagem" src="../img/h1-p-acesso.svg" alt="Titulocomimagem">
     </h1>
     </div>
             <h3>Bem-vindo <?php echo $nome_user; ?>, esse é o seu primeiro acesso. Informe seus dados.</h3>
     <h6>todos os campos com * são obrigatórios</h6>
-<form method="post" action="../controller/dados_alterados.php">
 
-    <!-- Adicione campos para coletar informações adicionais -->
-    <label>Nome Completo:</label>
-    <input type="text" name="nome_comp" placeholder="Digite seu nome completo" required>
+<div class="container">    
+    <form method="post" action="../controller/dados_alterados.php">
 
-    <label>CPF:</label>
-    <input type="text" name="cpf" placeholder="Apenas numeros" required>
+        <label>Nome Completo:</label>
+        <input type="text" name="nome_comp" placeholder="Digite seu nome completo" required>
 
-    <label>Data de Nascimento:</label>
-    <input type="date" name="dt_nasc" required>
+        <label>CPF:</label>
+        <input type="text" name="cpf" placeholder="Apenas numeros" required>
 
-    <label>E-mail:</label>
-    <input type="email" name="email" placeholder="Email particular" required>
+        <label>Data de Nascimento:</label>
+        <input type="date" name="dt_nasc" required>
 
-    <label>Telefone:</label>
-    <input type="text" name="telefone" placeholder="Exemplo: (xx) x xxxx-xxxx" required>
+        <label>E-mail:</label>
+        <input type="email" name="email" placeholder="Email particular" required>
 
-    <label>Cargo:</label>
-    <input type="text" name="cargo" required>
+        <label>Telefone:</label>
+        <input type="text" name="telefone" placeholder="Exemplo: (xx) x xxxx-xxxx" required>
 
-    <label>Identificação do Cargo:</label>
-    <input type="text" name="id_cargo" placeholder="Matricula ou Certificado" required>
+        <label>Cargo:</label>
+        <input type="text" name="cargo" required>
 
-    <label>Nova Senha:</label>
-    <input type="text" name="senha" placeholder="Escolha uma nova senha" required>
+        <label>Identificação do Cargo:</label>
+        <input type="text" name="id_cargo" placeholder="Matricula ou Certificado" required>
 
-    <button type="submit">Concluir Cadastro</button>
+        <label>Nova Senha:</label>
+        <input type="text" name="senha" placeholder="Escolha uma nova senha" required>
 
-    <script src="../js/mascaras.js"></script>
-</form>
+        <button type="submit">Concluir Cadastro</button>
 
+        <script src="../js/mascaras.js"></script>
+    </form>
+</div>
 </body>
 </html>
