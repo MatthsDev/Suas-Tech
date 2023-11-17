36bb128b457d71bce89e0f875499d687c83570af
MatthsDev <137323572+MatthsDev@users.noreply.github.com>
2023-11-17 11:58:20 -0300

    2

---
+++ cadunico/views/atendimento/ajax/listar_tela.php
@@ -0,0 +1,36 @@
+<?php
+require_once("../../../../config/conexao.php");
+
+echo '
+<table class="table table-sm mt-3">
+    <thead class="thead-light">
+        <tr>
+            <th scope="col">Senha</th>
+        </tr>
+    </thead>
+    <tbody>';
+
+// Execute a consulta SQL
+$res_todos = $pdo->query("SELECT senger.id, sen.nome_senha
+FROM senhas_geradas AS senger
+INNER JOIN senhas AS sen ON sen.id = senger.senha_id
+WHERE senger.sits_senha_id = 4
+ORDER BY senger.modified DESC
+LIMIT 1;");
+
+// Fetch os resultados
+$dados_total = $res_todos->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($dados_total as $row) {
+    $id = $row['id'];
+    $nome_senha = $row['nome_senha'];
+
+    echo '
+        <tr>
+            <td>' . $nome_senha . '</td>
+        </tr>';
+}
+
+echo '
+    </tbody>
+</table>';
--- cadunico/views/atendimento/index.php
+++ cadunico/views/atendimento/index.php
@@ -6,12 +6,13 @@ include_once "../../../config/conexao.php";
 
 <head>
     <meta charset="UTF-8" />
-    <title>Celke</title>
+    <title>Tech-Suas</title>
 </head>
 
 <body>
     <a href="gerar_atend.php">Gerar Senha</a><br>
     <a href="atendimento.php">Chamar Senha</a><br>
     <a href="liberar_senha.php">Liberar Senha</a><br>
+    <a href="tela.php">Liberar Senha</a><br>
 </body>
 </html>
