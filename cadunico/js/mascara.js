1cc2ab5bc7d0ca924691f12664083ee44f3676ec
MatthsDev <137323572+MatthsDev@users.noreply.github.com>
2023-11-17 13:18:26 -0300

    mascara js

--- cadunico/js/mascara.js
+++ cadunico/js/mascara.js
@@ -1,6 +1,7 @@
- $(document).ready(function(){
-      $('#telefone').mask('(00) 00000-0000');
-      $('#cpf').mask('000.000.000-00');
-      
-
-      });
+$(document).ready(function () {
+     // Aplicar a máscara para o CPF
+     $('#cpf').mask('000.000.000-00');
+ 
+     // Aplicar a máscara para o telefone
+     $('#telefone').mask('(00) 00000-0000');
+   });
--- cadunico/painel-usuario/primeiro_acesso.php
+++ cadunico/painel-usuario/primeiro_acesso.php
@@ -8,12 +8,14 @@ $_SESSION['nome_user_1_acesso'] = $nome_user;
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TechSUAS - Menu</title>
     <link rel="stylesheet" href="../css/adm.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="website icon" type="png" href="../img/logo.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
@@ -22,43 +24,48 @@ $_SESSION['nome_user_1_acesso'] = $nome_user;
 
 <body>
     <div class="img">
-    <h1 class="titulo-com-imagem">
-        <img class="titulo-com-imagem" src="../img/h1-primeiro.svg" alt="Titulocomimagem">
-    </h1>
+        <h1 class="titulo-com-imagem">
+            <img class="titulo-com-imagem" src="../img/h1-primeiro.svg" alt="Titulocomimagem">
+        </h1>
     </div>
-            <h3>Bem-vindo <?php echo $nome_user; ?>, esse é o seu primeiro acesso. Informe seus dados.</h3>
+    <h3>Bem-vindo
+        <?php echo $nome_user; ?>, esse é o seu primeiro acesso. Informe seus dados.
+    </h3>
     <h6>todos os campos com * são obrigatórios</h6>
-<form method="post" action="../controller/dados_alterados.php">
+    <form method="post" action="../controller/dados_alterados.php">
 
-    <!-- Adicione campos para coletar informações adicionais -->
-    <label>Nome Completo:</label>
-    <input type="text" name="nome_comp" placeholder="Digite seu nome completo" required>
+        <!-- Adicione campos para coletar informações adicionais -->
+        <label>Nome Completo:</label>
+        <input type="text" name="nome_comp" placeholder="Digite seu nome completo" required>
 
-    <label>CPF:</label>
-    <input type="text" name="cpf" placeholder="Apenas numeros" required>
+        <label>CPF:</label>
+        <input type="text" name="cpf" placeholder="Apenas numeros" required>
 
-    <label>Data de Nascimento:</label>
-    <input type="date" name="dt_nasc" required>
+        <label>Data de Nascimento:</label>
+        <input type="date" name="dt_nasc" required>
 
-    <label>E-mail:</label>
-    <input type="email" name="email" placeholder="Email particular" required>
+        <label>E-mail:</label>
+        <input type="email" name="email" placeholder="Email particular" required>
 
-    <label>Telefone:</label>
-    <input type="text" name="telefone" placeholder="Exemplo: (xx) x xxxx-xxxx" required>
+        <label>Telefone:</label>
+        <input type="text" name="telefone" placeholder="Exemplo: (xx) x xxxx-xxxx" required>
 
-    <label>Cargo:</label>
-    <input type="text" name="cargo" required>
+        <label>Cargo:</label>
+        <input type="text" name="cargo" required>
 
-    <label>Identificação do Cargo:</label>
-    <input type="text" name="id_cargo" placeholder="Matricula ou Certificado" required>
+        <label>Identificação do Cargo:</label>
+        <input type="text" name="id_cargo" placeholder="Matricula ou Certificado" required>
 
-    <label>Nova Senha:</label>
-    <input type="text" name="senha" placeholder="Escolha uma nova senha" required>
+        <label>Nova Senha:</label>
+        <input type="text" name="senha" placeholder="Escolha uma nova senha" required>
 
-    <button type="submit">Concluir Cadastro</button>
+        <button type="submit">Concluir Cadastro</button>
 
-    <script src="../js/mascaras.js"></script>
-</form>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="../js/jquery.mask.min.js"></script>
+        <script src="../js/mascaras.js"></script>
+    </form>
 
 </body>
+
 </html>
