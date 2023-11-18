640cc94916054f96ebc95e5adc6d1c943a1ef274
emmanuelcadete <engmecanicocadete@gmail.com>
2023-11-18 13:09:03 -0300

    Verifica se já exeste o nome de usuário.

--- cadunico/controller/processo_cad_user.php
+++ cadunico/controller/processo_cad_user.php
@@ -23,23 +23,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user_name = $_POST['nome_user'];
     $setor = $_POST['setor'];
     $funcao = $_POST['funcao'];
-//    $email = $_POST['email'];
+    $email = $_POST['email'];
 
-    // Adiciona a data e hora atual ao SQL
-    $smtp = $conn->prepare("INSERT INTO usuarios (usuario, senha, nivel, setor, funcao, data_registro) VALUES (?,?,?,?, NOW())");
+    // Verifica se o nome de usuário já existe no banco de dados
+    $verifica_usuario = $conn->prepare("SELECT usuario FROM usuarios WHERE usuario = ?");
+    $verifica_usuario->bind_param("s", $user_name);
+    $verifica_usuario->execute();
+    $verifica_usuario->store_result();
+
+    if ($verifica_usuario->num_rows > 0) {
+        // Se o nome de usuário já está em uso, exibe uma mensagem ou redirecione de volta ao formulário
+        echo '<script>alert("Nome de usuário já em uso. Por favor, escolha outro."); window.location.href = "../painel-adm/cadastro_user.php";</script>';
+        exit();
+    }
+
+    // Caso o Nome do Usuário seja unico será adicionado ao SQL
+    $smtp = $conn->prepare("INSERT INTO usuarios (usuario, senha, nivel, setor, funcao, email, data_registro) VALUES (?,?,?,?, NOW())");
 
     // Verifica se a preparação foi bem-sucedida
     if ($smtp === false) {
         die('Erro na preparação SQL: ' . $conn->error);
     }
 
-    $smtp->bind_param("sssss", $user_name, $senha_hashed, $tpacesso, $setor, $funcao);
+    $smtp->bind_param("ssssss", $user_name, $senha_hashed, $tpacesso, $setor, $funcao, $email);
 
     if ($smtp->execute()) {?>
         <h1>DADOS ENVIADOS COM SUCESSO!</h1>
         <div class="linha"></div>
         <?php
-        // Redireciona para a página DE CADASTRAR NOVO USUÁRIO após ALGUNS segundos
+// Redireciona para a página DE CADASTRAR NOVO USUÁRIO após ALGUNS segundos
         echo '<script> setTimeout(function(){ window.location.href = "../painel-adm/cadastro_user.php"; }, 1500); </script>';
     } else {
         echo "ERRO no envio dos DADOS: " . $smtp->error;
