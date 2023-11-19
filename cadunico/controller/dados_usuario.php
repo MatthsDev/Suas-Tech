e73699f5872b130b15151f97f8e43a884e86a77e
emmanuelcadete <engmecanicocadete@gmail.com>
2023-11-19 01:40:04 -0300

    Dados do usuario

--- cadunico/controller/dados_usuario.php
+++ cadunico/controller/dados_usuario.php
@@ -1,5 +1,31 @@
 <?php
-require_once '../../config/conexao.php'
+require_once '../../../config/conexao.php';
 
+$nomeUsuario = $_SESSION['user_usuario'];
 
+$sql = $pdo->prepare("SELECT * FROM usuarios WHERE usuario = :nomeUsuario");
+$sql->execute(array(':nomeUsuario' => $nomeUsuario));
+
+if ($sql->rowCount() > 0) {
+
+    $dados = $sql->fetch(PDO::FETCH_ASSOC);
+    $nome = $dados['nome'];
+    $apelido = $dados['apelido'];
+    $cpf = $dados['cpf'];
+    $setor = $dados['setor'];
+    $funcao = $dados['funcao'];
+    if ($funcao == 1){
+        $func = "Coordenação";
+    }elseif($funcao == 2){
+        $func = "Tecnico";
+    }else{
+        $func = "";
+    }
+    $dtNasc = $dados['dt_nasc'];
+    $telefone = $dados['telefone'];
+    $email = $dados['email'];
+    $cargo = $dados['cargo'];
+    $idcargo = $dados['id_cargo'];
+    
+}
 ?>
