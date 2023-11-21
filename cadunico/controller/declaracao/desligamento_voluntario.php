dd506c384beed96991aec86a835bb39ff6b27688
emmanuelcadete <engmecanicocadete@gmail.com>
2023-11-20 23:20:54 -0300

    funcionando porem falta alguns ajustes

--- cadunico/controller/declaracao/desligamento_voluntario.php
+++ cadunico/controller/declaracao/desligamento_voluntario.php
@@ -3,28 +3,15 @@
 
 <head>
     <style>
+
+
     body {
         font-family: Arial, sans-serif;
-        margin: 0;
-        padding: 0;
+
         background-image: url('../../img/marca.png');
-        background-size: cover;
-        background-repeat: no-repeat;
-    }
+        background-size: 790px;
+        background-repeat: repeat;
 
-    select {
-        width: auto;
-        /* Define a largura para automática */
-    }
-
-    /* Seu estilo adicional aqui */
-
-    #form-container {
-        width: 600px;
-        margin: 0 auto;
-        padding: 20px;
-        background-color: rgba(255, 255, 255, 0.9);
-        /* Adicione um fundo branco semitransparente ao formulário */
     }
 
     #title {
@@ -35,14 +22,10 @@
 
     #subtitle {
         font-size: 16px;
-        font-weight: bold;
         text-align: center;
         margin-top: 10px;
     }
 
-    #form {
-        margin-top: 20px;
-    }
 
     label {
         display: block;
@@ -50,15 +33,11 @@
         font-weight: bold;
     }
 
-    select,
-    input,
+
     textarea {
         width: 100%;
         padding: 5px;
         margin-top: 5px;
-    }
-
-    textarea {
         height: 100px;
     }
 
@@ -79,6 +58,7 @@
         text-indent: 50px;
         margin-top: 20px;
     }
+
     .conteudo{
     display:block;
     padding: 0px 5px 0px 30px;
@@ -93,11 +73,7 @@
 </head>
 
 <body>
-    <br><br><br><br><br><br>
-    <div id="title">DECLARAÇÃO DE DESLIGAMENTO VOLUNTÁRIO DO PROGRAMA BOLSA FAMÍLIA</div>
-
-    <h2>(Base legal: inc. XVII do caput do art. 24 e §§ 6º a 8º do art. 27 da Portaria MDS nº 897/2023)</h2>
-
+    <br><br><br><br><br>
     <?php
 ini_set('memory_limit', '256M');
 
@@ -105,10 +81,75 @@ setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 
 require_once "../../../config/conexao.php";
 
-if(isset($_POST['nis_dec'])){
-    $opcao = $_POST['nis_dec'];
-
-        $sql = $pdo->prepare("SELECT * FROM tbl_tudo WHERE num_cpf_pessoa = :cpf_dec");
-        $sql->execute(array(':cpf_dec' => $cpf_dec));
-
+if (!isset($_SESSION)) {
+    session_start();
 }
+include '../dados_usuario.php';
+
+if (isset($_POST['nis_dec'])) {
+    $nis_dec = $_POST['nis_dec'];
+
+    $sql = $pdo->prepare("SELECT * FROM tbl_tudo WHERE num_nis_pessoa_atual = :nis_dec");
+    $sql->execute(array(':nis_dec' => $nis_dec));
+
+    if ($sql->rowCount() > 0) {
+        $dados = $sql->fetch(PDO::FETCH_ASSOC);
+        $nom_pessoa = $dados["nom_pessoa"];
+        $cpf_rf = $dados['num_cpf_pessoa'];
+        $cpf_formatando = sprintf('%011s', $cpf_rf);
+        $cpf_formatado = substr($cpf_formatando, 0, 3) . '.' . substr($cpf_formatando, 3, 3) . '.' . substr($cpf_formatando, 6, 3) . '-' . substr($cpf_formatando, 9, 2);
+        $timestampptbr = time();
+        $data_formatada_at = strftime('%d de %B de %Y', $timestampptbr);
+
+        ?>
+        <div id="title">DECLARAÇÃO DE DESLIGAMENTO VOLUNTÁRIO DO PROGRAMA BOLSA FAMÍLIA</div>
+
+<h4>(Base legal: inc. XVII do caput do art. 24 e §§ 6º a 8º do art. 27 da Portaria MDS nº 897/2023)</h4>
+
+
+        <form id="form">
+        <div id="justified-text" class="conteudo">
+<p>Prezado(a) coordenador(a) municipal do Programa Bolsa Família de São Bento do Una / PE,</p>
+<p>Eu, <b><?php echo $nom_pessoa; ?></b> beneficiária(o) do Programa Bolsa Família (PBF), solicito meu desligamento voluntário do referido Programa, com a atualização cadastral no Cadastro Único para Programas Socias do Governo Federal (CadÚnico) e o registro da minha renda atual e/ou outras informações relevantes para o meu cadastro.</p>
+<p>Declaro, ainda, que:</p>
+
+<p>Estou ciente de que poderei, a qualquer momento dentro do prazo de 36 meses, solicitar meu retorno ao Programa Bolsa Família, mediante nova atualização cadastral que comprove minha necessidade socioeconômica para participar novamente do Programa.</p>
+<p>Estou ciente de que esse retorno ao Programa não gera o pagamento das parcelas anteriormente canceladas, e que apenas poderei receber as parcelas geradas a partir do processamento de minha nova inclusão no PBF</p>
+<br><br>
+<p>Dados do(a) Responsável Familiar:</p>
+<?php
+echo "Nome completo: " . $nom_pessoa . "<br>";
+        echo "CPF: " . $cpf_formatado . "<br>";
+        echo "NIS: " . $nis_dec . "<br>";
+        ?>
+    <br><br>
+<p style='text-align:right;'>São Bento do Una, <?php echo $data_formatada_at; ?></p>
+<br><br>
+<p style='text-align:center;'>__________________________________________________________________________</p>
+<p style='text-align:center;'>Assinatura do(a) Responsável Familiar</p>
+</din>
+
+<div id="justified-text" class="conteudo">
+<p style="page-break-before: always;">Eu, coordenador(a) municipal do Programa Bolsa Família de São Bento do Una/PE, ou por ele designado, afirmo que foi realizada, nesta data, a atualização cadastral do(a) beneficiário(a) acima identificado(a) no Cadastro Único para Programas Socias do Governo Federal (CadÚnico), e o cancelamento do benefício do Programa Bolsa Família, no Sistema de Benefícios ao Cidadão (Sibec), pelo motivo “Desligamento voluntário”.</p>
+<p>Declaro, ainda, que procedi ao cancelamento apenas do benefício da família, e não a exclusão de seu cadastro.</p>
+<p>O(a) responsável familiar acima identificado(a) poderá ter o cancelamento do seu benefício revertido dentro do prazo de 36 meses (três anos), retornando imediatamente à condição de beneficiário(a), caso sua renda volte a ser compatível com as regras do Programa Bolsa Família. Basta, para isso, que seus dados sejam atualizados no CadÚnico, e que o pedido de retorno garantido seja feito ao coordenador municipal do Programa.</p>
+<p>A presente declaração foi assinada em duas vias, uma arquivada no município e a outra entregue para o(a) beneficiário(a).</p>
+<br><br>
+<p>Dados do(a) coordenador(a) municipal do PBF:</p>
+<?php echo "Nome completo: " . $_SESSION['nome_usuario'] . "<br>";
+        echo "CPF: " . $cpf . "<br>";
+        ?>
+    <br><br>
+<p style='text-align:right;'>São Bento do Una, <?php echo $data_formatada_at; ?></p>
+<br><br>
+<p style='text-align:center;'>__________________________________________________________________________</p>
+<p style='text-align:center;'>Assinatura do(a) coordenador(a) municipal do Programa Bolsa Família</p>
+</div>
+</form>
+<?php
+} else {
+        echo "<script language='javascript'>window.alert('Nenhum dado encontrato.!'); </script>";
+        echo "<script language='javascript'>window.location='../../views/declar/declaracao.php'; </script>";
+    }
+}
+?>
