63a2517d1016bce68e97fea258aafa7ca27d83aa
MatthsDev <137323572+MatthsDev@users.noreply.github.com>
2023-11-22 20:05:51 -0300

    s

--- config/autenticar.php
+++ config/autenticar.php
@@ -8,50 +8,38 @@ if (empty($_POST['usuario']) || empty($_POST['senha'])) {
 }
 
 $usuario = $_POST['usuario'];
-$senha = $_POST['senha'];
+$senha_login = $_POST['senha'];
 
-$res = $pdo->prepare("SELECT * from usuarios where usuario = :usuario and senha = :senha ");
-$res->bindValue(":usuario", $usuario);
-$res->bindValue(":senha", $senha);
-$res->execute();
+$stmt = $pdo->prepare("SELECT * FROM usuarios WHERE usuario = :usuario");
+$stmt->bindValue(":usuario", $usuario);
+$stmt->execute();
 
-$dados = $res->fetchAll(PDO::FETCH_ASSOC);
-$linhas = count($dados);
+$dados = $stmt->fetch(PDO::FETCH_ASSOC);
 
-if ($linhas > 0) {
-    $_SESSION['nome_usuario'] = $dados[0]['nome'];
-    $_SESSION['user_usuario'] = $dados[0]['usuario'];
-    $_SESSION['nivel_usuario'] = $dados[0]['nivel'];
+if ($dados && password_verify($senha_login, $dados['senha'])) {
+    $_SESSION['nome_usuario'] = $dados['nome'];
+    $_SESSION['user_usuario'] = $dados['usuario'];
+    $_SESSION['nivel_usuario'] = $dados['nivel'];
 
     // Verifique se é o primeiro acesso pela senha
-    if ($senha == "@senha123") {
+    if ($senha_login == "@senha123") {
         // Passa o nome do usuário para a página de primeiro acesso
-        $_SESSION['nome_user_1_acesso'] = $dados[0]['usuario'];
+        $_SESSION['nome_user_1_acesso'] = $dados['usuario'];
 
         // Redirecione para a página de conclusão do cadastro
-        header("Location: ../cadunico/painel-usuario/primeiro_acesso.php");
+        header("Location: ../cadunico/views/acessos/primeiro_acesso.php");
         exit();
     }
 
+    // Redirecione com base no nível de acesso
     if ($_SESSION['nivel_usuario'] == 'admin') {
         header("location:../cadunico/painel-adm/adm-view.php");
         exit();
-    }
-
-    if ($_SESSION['nivel_usuario'] == 'usuario') {
+    } elseif ($_SESSION['nivel_usuario'] == 'usuario') {
         header("location:../cadunico/painel-usuario/user-painel.php");
         exit();
     }
-
 } else {
-    echo "<script language='javascript'>window.alert('Dados Incorretos!!'); </script>";
+    echo "<script language='javascript'>window.alert('Senha incorreta!'); </script>";
     echo "<script language='javascript'>window.location='../index.php'; </script>";
-}
-
-//FUNÇÃO BOTAO
-
-if (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] == 'admin') {
-    echo '<a href="../painel-adm/adm-view.php">Ir para o Painel de Admin</a>';
-} elseif (isset($_SESSION['nivel_usuario']) && $_SESSION['nivel_usuario'] == 'usuario') {
-    echo '<a href="../painel-usuario/user-painel.php">Ir para o Painel de Usuário</a>';
 }
