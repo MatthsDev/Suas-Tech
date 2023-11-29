ee4870f859552a0994d2a26f603f865cb0be567f
emmanuelcadete <engmecanicocadete@gmail.com>
2023-11-29 12:11:47 -0300

    print

--- cadunico/controller/declaracao/print_enc.php
+++ cadunico/controller/declaracao/print_enc.php
@@ -21,6 +21,7 @@
                     border-radius: 5px;
                 }
 </style>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Imprimir encaminhamento</title>
