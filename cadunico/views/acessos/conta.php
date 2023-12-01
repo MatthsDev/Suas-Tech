e53f84b28e48f69af08718a37d5156a1fb89362e
emmanuelcadete <engmecanicocadete@gmail.com>
2023-12-01 12:29:12 -0300

    fasdf

--- cadunico/views/acessos/conta.php
+++ cadunico/views/acessos/conta.php
@@ -12,7 +12,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/cadunico/controller/acesso_
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/style-alt-user.css">
-    <link rel="website icon" type="image/png" href="../img/logo.png">
+    <link rel="website icon" type="image/png" href="../../img/logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
     <title>Sala do Usuário</title>
