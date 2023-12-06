727e572f39096c01a6dfea8277f3c34ce62de967
emmanuelcadete <engmecanicocadete@gmail.com>
2023-12-06 11:26:22 -0300

    farkart

--- suas/views/atualizar_tabela.php
+++ suas/views/atualizar_tabela.php
@@ -1,15 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="website icon" type="png" href="../../cadunico/img/logo.png">
     <title>Importar CSV</title>
 </head>
 <body>
-    <form action="../../controller/importar.php" method="post" enctype="multipart/form-data">
-        Selecione o arquivo CSV: <input type="file" name="arquivoCSV" accept=".csv">
-        <input type="submit" value="Importar">
+    <form action="import.php" method="POST" enctype="multipart/form-data">
+
+        <labal>Qual tabela você pretende atualizar: </label>
+        <select name="csv_tbl" required>
+        <option value="tudo">Base de Dados do Cadastro Único</option>
+        <option value="folha">Folha de Pagamento</option>
+
+    </select>
+
+       Selecione o arquivo CSV: <input type="file" name="arquivoCSV" id="arquivoCSV" accept=".csv">
+                                <input type="submit" value="Importar">
+
     </form>
 </body>
 </html>
