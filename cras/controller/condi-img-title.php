6c7757123a187d336a85c4455e723899912c772d
emmanuelcadete <engmecanicocadete@gmail.com>
2023-12-10 21:50:52 -0300

    org

--- cras/controller/condi-img-title.php
+++ cras/controller/condi-img-title.php
@@ -1,5 +1,7 @@
 <?php
-include_once '../../cadunico/controller/acesso_user/dados_usuario.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/config/conexao.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/config/sessao.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/cadunico/controller/acesso_user/dados_usuario.php';
 
 
 $tituloCras1 = "Cras Antonio Matias";
