8c8e3669e731b2eb3f45f67ffde048cb96e56ce5
emmanuelcadete <engmecanicocadete@gmail.com>
2023-12-12 09:49:39 -0300

    vads

--- cras/controller/encaminhamento.php
+++ cras/controller/encaminhamento.php
@@ -13,15 +13,50 @@ setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 if (isset($_POST['buscar_dados'])) {
 
     $cpf_nis = $_POST['buscar_dados'];
-    $destino_enc = $_POST['nom_localidade_fam'];
+
 
     if ($cpf_nis == 'cpf_dec') {
-        $dados = $pdo->prepare("SELECT * FROM tbl_tudo WHERE num_cpf_pessoa = :cpf_dec");
-        $dados->execute(array(":cpf_dec" => $cpf_nis));
+        $cpf = $_POST['valorescolhido'];
+        $sql = $pdo->prepare("SELECT * FROM tbl_tudo WHERE num_cpf_pessoa = :valorescolhido");
+        $sql->execute(array(':valorescolhido' => $cpf));
+
+            if($sql->rowCount() > 0){
+                $dados = $sql->fetch(PDO::FETCH_ASSOC);
+                $local = $dados['nom_localidade_fam'];
+                $local = $dados['nom_pessoa'];
+                $local = $dados['nom_localidade_fam'];
+                $local = $dados['nom_localidade_fam'];
+                $local = $dados['nom_localidade_fam'];
+                $local = $dados['nom_localidade_fam'];
+                $local = $dados['nom_localidade_fam'];
+
+                echo $local . $dados['nom_pessoa'];
+
+            }else{
+
+            }
+
 
     } elseif ($cpf_nis == 'nis_dec') {
-        $dados = $pdo->prepare("SELECT * FROM tbl_tudo WHERE num_nis_pessoa_atual = :nis_dec");
-        $dados->execute(array(":nis_dec" => $cpf_nis));
+        $nis = $_POST['valorescolhido'];
+        $sql = $pdo->prepare("SELECT * FROM tbl_tudo WHERE num_nis_pessoa_atual = :valorescolhido");
+        $sql->execute(array(':valorescolhido' => $nis));
+        
+        if($sql->rowCount() > 0){
+            $dados = $sql->fetch(PDO::FETCH_ASSOC);
+            $local = $dados['nom_localidade_fam'];
+            $local = $dados['nom_pessoa'];
+            $local = $dados['nom_localidade_fam'];
+            $local = $dados['nom_localidade_fam'];
+            $local = $dados['nom_localidade_fam'];
+            $local = $dados['nom_localidade_fam'];
+            $local = $dados['nom_localidade_fam'];
+
+            echo $local . $dados['nom_pessoa'];
+
+        }else{
+
+        }
     }
 
 }
