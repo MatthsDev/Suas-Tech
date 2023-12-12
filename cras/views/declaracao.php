8a1333df7063c856d22c4d2b40a3a915faa659e9
Miguel4morim <152441542+Miguel4morim@users.noreply.github.com>
2023-12-12 12:30:53 -0300

    1

--- cras/views/declaracao.php
+++ cras/views/declaracao.php
@@ -14,50 +14,52 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/config/sessao.php';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <title>Declarações</title>
 </head>
+
 <body>
     <div class="img">
         <h1 class="titulo-com-imagem">
             <img src="../../img/h1-declaração.svg" alt="Titulocomimagem">
         </h1>
     </div>
-            <!--INICIO DA DECLARAÇÃO PARA ENCAMINHAMENTO-->
-        <div class="encaminhamento">
-            <form method="post" action="controller/encaminhamento.php">
-                <h2>Encaminhamento</h2>
-                <select name="buscar_dados" required>
-                    <option value="cpf_dec">CPF:</option>
-                    <option value="nis_dec">NIS:</option>
-                </select>
-                <input type="text" name="valorescolhido" placeholder="Digite aqui:" required>
+    <!--INICIO DA DECLARAÇÃO PARA ENCAMINHAMENTO-->
+    <div class="encaminhamento">
+        <form method="post" action="controller/encaminhamento.php">
+            <h2>Encaminhamento</h2>
+            <select name="buscar_dados" required>
+                <option value="cpf_dec">CPF:</option>
+                <option value="nis_dec">NIS:</option>
+            </select>
+            <input type="text" name="valorescolhido" placeholder="Digite aqui:" required>
 
-                <label>Encaminhar para: </label>
-                <select name="setor" required>
-        <option value="" disabled selected hidden>Selecione</option>
-        <?php
+            <label>Encaminhar para: </label>
+            <select name="setor" required>
+                <option value="" disabled selected hidden>Selecione</option>
+                <?php
 
-            $consultaSetores = $conn->query("SELECT instituicao, nome_instit FROM setores");
+                $consultaSetores = $conn->query("SELECT instituicao, nome_instit FROM setores");
 
-            // Verifica se há resultados na consulta
-            if ($consultaSetores->num_rows > 0) {
-                
-                // Loop para criar as opções do select
-                while ($setor = $consultaSetores->fetch_assoc()) {
-                    echo '<option value="' . $setor['instituicao'] . ' - ' . $setor['nome_instit'] . '">' . $setor['instituicao'] . ' - ' . $setor['nome_instit'] . '</option>';
+                // Verifica se há resultados na consulta
+                if ($consultaSetores->num_rows > 0) {
+
+                    // Loop para criar as opções do select
+                    while ($setor = $consultaSetores->fetch_assoc()) {
+                        echo '<option value="' . $setor['instituicao'] . ' - ' . $setor['nome_instit'] . '">' . $setor['instituicao'] . ' - ' . $setor['nome_instit'] . '</option>';
+                    }
                 }
-            }
-            ?>
-    </div> 
-    </select>
-                <label>Texto: </label>
-                <textarea id="" name="texto" required  oninput="ajustarTextarea(this)"></textarea>
-                <button type="submit">BUSCAR</button>
-            </form>
-<script>
-    function ajustarTextarea(textarea) {
-    textarea.style.height = 'auto';
-    textarea.style.height = textarea.scrollHeight + 'px';
-    }
-</script>
+                ?>
+
+            </select>
+            <label>Texto: </label>
+            <textarea id="" name="texto" required oninput="ajustarTextarea(this)"></textarea>
+            <button type="submit">BUSCAR</button>
+        </form>
+    </div>
+    <script>
+        function ajustarTextarea(textarea) {
+            textarea.style.height = 'auto';
+            textarea.style.height = textarea.scrollHeight + 'px';
+        }
+    </script>
 </body>
 
 </html>
