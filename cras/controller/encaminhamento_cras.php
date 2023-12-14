90358b1e025af36bafb7c690f4390f7d08d43670
Miguel4morim <152441542+Miguel4morim@users.noreply.github.com>
2023-12-14 13:14:32 -0300

    .

--- cras/controller/encaminhamento_cras.php
+++ cras/controller/encaminhamento_cras.php
@@ -28,12 +28,12 @@
             //Dados do formulário
             $cpf_dec = $_POST['valorescolhido'];
             $texto = $_POST['texto'];
-            $direcao = $_POST['setor'];
+            $local = $_POST['setor'];
 
             //dados da tabela com todos os cadastros
 
             // Consulta preparada para evitar injeção de SQL
-            $sql = $pdo->prepare("SELECT * FROM cras WHERE cpf = :cpf_dec");
+            $sql = $pdo->prepare("SELECT * FROM cras_test WHERE cpf = :cpf_dec");
             $sql->execute(array(':cpf_dec' => $cpf_dec));
 
 
@@ -45,16 +45,15 @@
 
             if ($sql->rowCount() > 0) {
                 $dados = $sql->fetch(PDO::FETCH_ASSOC);
-                $cod_familiar = $dados["cod_familiar_fam"];
-                $nom_pessoa = $dados["nom_pessoa"];
-                $nom_mae_rf = $dados["nom_completo_mae_pessoa"];
-                $sexo_pessoa_ = $dados["cod_sexo_pessoa"];
+
+                $nom_pessoa = $dados["nome"];
+
 
                
                 }
                 //Formatando o CPF
-                $cpf_table = $dados['num_cpf_pessoa'];
-                $cpf_formatando = sprintf('%011s', $cpf_table);
+            
+                $cpf_formatando = sprintf('%011s', $cpf_dec);
                 $cpf_formatado = substr($cpf_formatando, 0, 3) . '.' . substr($cpf_formatando, 3, 3) . '.' . substr($cpf_formatando, 6, 3) . '-' . substr($cpf_formatando, 9, 2);
 
                 //Define as variáveis com o endereço
@@ -63,12 +62,12 @@
                 $numero = $dados['numero'];
                 $referencia = $dados['referencia'];
 
-                $enderecoCompleto = $bairro . " " . $logradouro . " " . $numero . ", " . $referencia;
+                $enderecoCompleto = $bairro . ", " . $logradouro . ", " . "n° $numero" . ", " . $referencia;
 
-                $conteudo = "<div id='title'style='margin-top: 100px;'>CADASTRO ÚNICO - SÃO BENTO DO UNA</div>";
+                $conteudo = "<div id='title'style='margin-top: 100px;'> $local - SÃO BENTO DO UNA</div>";
                 $conteudo .= "<br><br><p class='right-align'>São Bento do Una, " . $data_formatada_at . "</p>";
-                $conteudo .= "<br><br><p>Assunto: Encaminho" . $nom_pessoa . ", CPF: " . $cpf_formatado . ", reside em " . $enderecoCompleto . ".</p>";
-                $conteudo .= "<p>Ao(a) Coordenador(a) do " . $direcao . ",</p>";
+                $conteudo .= "<br><br><p>Assunto: Encaminho " . $nom_pessoa . ", CPF: " . $cpf_formatado . ", reside em " . $enderecoCompleto . ".</p>";
+                $conteudo .= "<p>Ao(a) Coordenador(a) do " . $local . ",</p>";
                 $conteudo .= "<p>" . $texto . "</p>";
                 $conteudo .= "<p>Permaneço à disposição para quaisquer esclarecimentos adicionais que se façam necessários.</p>";
                 $conteudo .= "<p>Atenciosamente,</p>";
@@ -79,7 +78,7 @@
                 $conteudo .= "<p>_________________________________________________________________________________________________________</p>";
                 $conteudo .= "<p>ASSINATURA</p>";
                 $conteudo .= "</div>";
-                header("Location: print_enc.php?conteudo=" . urlencode($conteudo));
+                header("Location: print_enc1.php?conteudo=" . urlencode($conteudo));
             }
         
         elseif ($opcao == "nis_dec") {
@@ -101,27 +100,10 @@
 
             if ($sql->rowCount() > 0) {
                 $dados = $sql->fetch(PDO::FETCH_ASSOC);
-                $cod_familiar = $dados["cod_familiar_fam"];
-                $nom_pessoa = $dados["nom_pessoa"];
-                $nom_mae_rf = $dados["nom_completo_mae_pessoa"];
-                $sexo_pessoa_ = $dados["cod_sexo_pessoa"];
+                
+                $nom_pessoa = $dados["nome"];
 
-                //Define as variáveis com o Female e Male
-                if ($sexo_pessoa_ == "1") {
-                    $sexo = " o Sr. ";
-                } else {
-                    $sexo = " a Sra. ";
-                }
-                if ($sexo_pessoa_ == "1") {
-                    $sexoo = " filho de ";
-                } else {
-                    $sexoo = " filha de ";
-                }
-                if ($sexo_pessoa_ == "1") {
-                    $sexooo = " inscrito ";
-                } else {
-                    $sexooo = " inscrita ";
-                }
+                
 
                 //Define as variáveis com o endereço
                 $bairro = $dados['bairro'];
@@ -129,12 +111,12 @@
                 $numero = $dados['numero'];
                 $referencia = $dados['referencia'];
 
-                $enderecoCompleto = $bairro . " " . $logradouro . " " . $numero . ", " . $referencia;
+                $enderecoCompleto = $bairro . ", " . $logradouro . ", " . $numero . ", " . $referencia;
 
-                $conteudo = "<div id='title'style='margin-top: 100px;'>CADASTRO ÚNICO - SÃO BENTO DO UNA</div>";
+                $conteudo = "<div id='title'style='margin-top: 100px;'> $local - SÃO BENTO DO UNA</div>";
                 $conteudo .= "<br><br><p class='right-align'>São Bento do Una, " . $data_formatada_at . "</p>";
-                $conteudo .= "<br><br><p>Assunto: Encaminho" . $nom_pessoa . ", CPF: " . $cpf_formatado . ", reside em " . $enderecoCompleto . ".</p>";
-                $conteudo .= "<p>Ao(a) Coordenador(a) do " . $direcao . ",</p>";
+                $conteudo .= "<br><br><p>Assunto: Encaminho" . " $nom_pessoa" . ", CPF: " . " $cpf_formatado" . ", reside em " . " $enderecoCompleto" . ".</p>";
+                $conteudo .= "<p>Ao(a) Coordenador(a) do " . "$local ,</p>";
                 $conteudo .= "<p>" . $texto . "</p>";
                 $conteudo .= "<p>Permaneço à disposição para quaisquer esclarecimentos adicionais que se façam necessários.</p>";
                 $conteudo .= "<p>Atenciosamente,</p>";
@@ -145,7 +127,7 @@
                 $conteudo .= "<p>_________________________________________________________________________________________________________</p>";
                 $conteudo .= "<p>ASSINATURA</p>";
                 $conteudo .= "</div>";
-                header("Location: print_enc.php?conteudo=" . urlencode($conteudo));
+                header("Location: print_enc1.php?conteudo=" . urlencode($conteudo));
             }
         } else {
             echo "Nâo encontrado.";
