1e889c7661e9687d367819d768be6998c8cd9586
emmanuelcadete <engmecanicocadete@gmail.com>
2023-12-17 22:58:58 -0300

    funcionando, mas faltando muita coisa ainda.

--- cozinha_comunitaria/views/fluxo_diario.php
+++ cozinha_comunitaria/views/fluxo_diario.php
@@ -17,12 +17,30 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/cadunico/controller/acesso_
 
 </head>
 <body>
+    <?php
+// Execute a query para somar os valores da coluna
+$sql_soma = "SELECT SUM(qtd_marmita) as soma_total FROM fluxo_diario_coz";
+$sqli_soma = "SELECT SUM(marm_entregue) as soma_total FROM fluxo_diario_coz";
+
+$resultado_soma = $conn->query($sql_soma);
+$resultado2_soma = $conn->query($sqli_soma);
+
+if ($resultado_soma && $resultado2_soma) {
+    $soma_total = $resultado_soma->fetch_assoc()['soma_total'];
+    $soma2_total = $resultado2_soma->fetch_assoc()['soma_total'];
+    $faltando = $soma_total - $soma2_total;
+    echo "Total de marmita(s): " . $soma_total . " para o dia de hoje.<br>";
+    echo "Faltam entregar: " . $faltando . " marmita(s) hoje.";
+} else {
+    echo "Erro ao calcular a soma: " . $conn->error;
+}
+?>
     <h1>FLUXO DIÁRiO</h1>
 
     <form action="">
     <label>Buscar Beneficiário: </label>
     <input type="text" id="nis" name="nis" placeholder="Pelo NIS." maxlength="11" required>
-
+</form>
     <?php
 if (!isset($_GET['nis'])) {
 
@@ -39,39 +57,69 @@ if (!isset($_GET['nis'])) {
         $dados = $sql_query->fetch_assoc();
         ?>
 
-NOME: <?php echo $dados['nome']; ?> CPF: <?php echo $dados['cpf_benef']; ?> <br>
+<br>NOME: <?php echo $dados['nome']; ?> <br>
+Quantidades de marmitas: <?php echo $dados['qtd_marmita']; ?> <br>
+CPF: <?php echo $dados['cpf_benef']; ?>
+<form method="post" action="">
+    <input type=text name="qtd">
+    <input type="checkbox" name="ok">
+    <button type="submit">Entregue</button>
+</form>
 
+<hr>
     <?php
-}
+if (!isset($_POST['ok'])) {
+
+        } else {
+            //data criada com formato 'DD de mmmm de YYYY'
+            $timestampptbr = time();
+            $data_formatada_at = strftime('%d de %B de %Y', $timestampptbr);
+            $get_rec = "ok";
+            $qtd_entregue = $_POST['qtd'];
+
+            
+
+            $sqld = $conn->prepare("UPDATE fluxo_diario_coz SET data_de_entrega=?, marm_entregue=?, entregue=? WHERE nis_benef=?");
+            $sqld->bind_param("ssss", $data_formatada_at, $qtd_entregue, $get_rec, $dados['nis_benef']);
+            if ($sqld->execute()) {
+                echo '<script>alert("Entrega registrada!"); window.location.href = "fluxo_diario.php";</script>';
+            } else {
+                echo "Não salvou" . $sqld->error . "contate o suporte.";
+            }
+        }
+    }
 }
 
-$tbl_fluxo = $conn->query("SELECT nis_benef, nome, encaminhado_cras, qtd_marmita FROM fluxo_diario_coz");
+$tbl_fluxo = $conn->query("SELECT nis_benef, nome, encaminhado_cras, qtd_marmita, entregue FROM fluxo_diario_coz");
 
-if($tbl_fluxo->num_rows > 0){
+if ($tbl_fluxo->num_rows > 0) {
+
     ?>
-    <br><br><br><br><table width="650px" border="1">
+    <br><br><table width="650px" border="1">
         <tr class="titulo" >
-                
-                <th class="cabecalho">NIS</th>    
+
+                <th class="cabecalho">NIS</th>
                 <th class="cabecalho">NOME</th>
-                <th class="cabecalho">CRAS</th>
+                <th class="cabecalho">ENCAMINHADO</th>
                 <th class="cabecalho">QUANTIDADES</th>
-                    
+                <th class="cabecalho">ENTREGUE</th>
+
             </tr>
     <?php
-            while($linha = $tbl_fluxo->fetch_assoc()){
+while ($linha = $tbl_fluxo->fetch_assoc()) {
         ?>
         <tr class="resultado">
         <td class="resultado"><?php echo $linha['nis_benef']; ?></td>
         <td class="resultado"><?php echo $linha['nome']; ?></td>
         <td class="resultado"><?php echo $linha['encaminhado_cras']; ?></td>
         <td class="resultado"><?php echo $linha['qtd_marmita']; ?></td>
+        <td class="resultado"><?php echo $linha['entregue']; ?></td>
     <?php
-    }
-}else{
+}
+} else {
     ?>
-    <tr>    
-    <td colspan="4">Resultados da pesquisa</td>   
+    <tr>
+    <td colspan="4">Resultados da pesquisa</td>
 </tr>
 <?php
 }
