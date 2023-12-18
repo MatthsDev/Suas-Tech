367acd34984e0d7a7bebac98f4c73ba17a3636d1
Diego-m-dev <diegomoreira.pab@gmail.com>
2023-12-18 18:02:47 -0300

    1

--- cras/views/declaracao.php
+++ cras/views/declaracao.php
@@ -109,7 +109,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/config/sessao.php';
             textarea.style.height = textarea.scrollHeight + 'px';
         }
     </script>
-    <script src='../../controller/back1.js'></script>
+    <script src='../../controller/back.js'></script>
     
 </body>
 
