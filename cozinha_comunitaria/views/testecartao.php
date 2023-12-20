fb158b78a30e8299bda384b4afc063d231f5a725
Diego-m-dev <diegomoreira.pab@gmail.com>
2023-12-20 13:44:25 -0300

    1

--- cozinha_comunitaria/views/testecartao.php
+++ cozinha_comunitaria/views/testecartao.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cartão Cozinha Comunitaria</title>
     <link rel="stylesheet" href="../css/stylecartao.css">
+    <link rel="shortcut icon" href="../img/logo.png" type="image/x-icon">
 </head>
 <body>
-    <img src="../img/CARTÃO COZINHA (4).png" alt="cartaocozinha">
+    <img src="../img/CARTÃO COZINHA.svg" alt="cartaocozinha">
 </body>
 </html>
