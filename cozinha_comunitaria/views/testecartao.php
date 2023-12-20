57a9b53f1b194a80dbd386e900f4eac48523ead9
Diego-m-dev <diegomoreira.pab@gmail.com>
2023-12-20 13:35:15 -0300

    1

--- cozinha_comunitaria/views/testecartao.php
+++ cozinha_comunitaria/views/testecartao.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cartão Cozinha Comunitaria</title>
+    <link rel="stylesheet" href="../css/stylecartao.css">
 </head>
 <body>
     <img src="../img/CARTÃO COZINHA (4).png" alt="cartaocozinha">
