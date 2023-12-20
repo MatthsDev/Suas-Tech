876131f24edb3e1770de3e6ed680798ad1a16ffa
Diego-m-dev <diegomoreira.pab@gmail.com>
2023-12-20 14:53:41 -0300

    1

--- suas/views/atualizar_tabela.php
+++ suas/views/atualizar_tabela.php
@@ -7,20 +7,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="website icon" type="png" href="../../cadunico/img/logo.png">
     <link rel="stylesheet" href="../css/stylebanco.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <title>Importar CSV</title>
 </head>
 
 <body>
-<body>
-    <div class="img">
-        <h1 class="titulo-com-imagem">
-            <img class="titulo-com-imagem" src="../img/h1-banco.svg" alt="Titulocomimagem">
-        </h1>
-    </div>
-    <div class="container">
-        <form action="../controller/import.php" method="POST" enctype="multipart/form-data">
 
-            <label>Qual tabela você pretende atualizar: </label>
+    <body>
+        <div class="img">
+            <h1 class="titulo-com-imagem">
+                <img class="titulo-com-imagem" src="../img/h1-banco.svg" alt="Titulocomimagem">
+            </h1>
+        </div>
+        <div class="container">
+            <form action="../controller/import.php" method="POST" enctype="multipart/form-data">
+
+                <label>Qual tabela você pretende atualizar: </label>
                 <select name="csv_tbl" required>
                     <option value="tudo">Base de Dados do Cadastro Único</option>
                     <option value="folha">Folha de Pagamento</option>
@@ -29,9 +31,14 @@
 
                 Selecione o arquivo CSV: <input type="file" name="arquivoCSV" id="arquivoCSV" accept=".csv">
                 <button type="submit" value="Importar"> Importar </button>
-
-        </form>
-    </div>
-</body>
+                <div class="btn">
+                    <a onclick="goBack()">
+                        <i class="fas fa-arrow-left"></i> Voltar ao menu
+                    </a>
+                </div>
+            </form>
+        </div>
+    </body>
+    <script src='../../controller/back.js'></script>
 
 </html>
