e93d7433075eb418ba2a572287d009ca83857fb9
Diego-m-dev <diegomoreira.pab@gmail.com>
2023-12-21 10:48:10 -0300

    1

--- cozinha_comunitaria/controller/tbl_fluxo.php
+++ cozinha_comunitaria/controller/tbl_fluxo.php
@@ -2,7 +2,12 @@
 
 include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/config/conexao.php';
 
-$tbl_fluxo = $conn->query("SELECT nis_benef, nome, encaminhado_cras, qtd_marmita, entregue, prioridade FROM fluxo_diario_coz ORDER BY prioridade DESC LIMIT 20");
+$tbl_fluxo = $conn->query("SELECT nis_benef, nome, encaminhado_cras, qtd_marmita, entregue FROM fluxo_diario_coz ORDER BY
+    CASE 
+        WHEN prioridade = 'urgente' THEN 1
+        WHEN prioridade = 'prioridade' THEN 2
+        ELSE 3
+    END, data_de_entrega ASC LIMIT 4");
 
 if ($tbl_fluxo->num_rows > 0) {
     ?>
@@ -34,7 +39,7 @@ $contador = 0;
 }
 
     // Verifica se há registros excedentes para a lista de espera
-    if ($contador >= 20) {
+    if ($contador >= 4) {
         echo "<tr><td colspan='6'>Lista de espera para registros excedentes.</td></tr>";
     }
 
@@ -56,10 +61,16 @@ $resultado2_soma = $conn->query($sqli_soma);
 
 if ($resultado_soma && $resultado2_soma) {
     $soma_total = $resultado_soma->fetch_assoc()['soma_total'];
+    if ($soma_total <= 0) {
+        $sum_all = 0;
+    } else {
+        $sum_all = $soma_total;
+    }
     $soma2_total = $resultado2_soma->fetch_assoc()['soma_total'];
     $faltando = $soma_total - $soma2_total;
-    echo "Total de marmita(s): " . $soma_total . " para o dia de hoje.<br>";
-    echo "Faltam entregar: " . $faltando . " marmita(s) hoje.";
+
+    echo "Total de marmita(s): <b>" . $sum_all . "</b> para o dia de hoje.<br>";
+    echo "Faltam entregar: <b>" . $faltando . "</b> marmita(s) hoje.";
 } else {
     echo "Erro ao calcular a soma: " . $conn->error;
 }
--- cozinha_comunitaria/views/testecartao.php
+++ cozinha_comunitaria/views/testecartao.php
@@ -1,3 +1,11 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/config/conexao.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/config/sessao.php';
+
+$dados_cartao = $conn->query("SELECT nis_benef, nome FROM fluxo_diario_coz");
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -8,6 +16,16 @@
     <link rel="shortcut icon" href="../img/logo.png" type="image/x-icon">
 </head>
 <body>
-    <img src="../img/CARTÃO COZINHA.svg" alt="cartaocozinha">
-</body>
+<?php
+    if ($dados_cartao->num_rows > 0) {
+        while ($linha = $dados_cartao->fetch_assoc()) {
+            echo "<div class='card'>";
+            echo "<div class='name'>" . $linha['nome'] . "</div>";
+            echo "<img src='../img/CARTÃO COZINHA.svg' alt='cartaocozinha'>";
+            echo "<div class='nis'>" . $linha['nis_benef'] . "</div>";
+            echo "</div>";
+        }
+    }
+    ?>
+    </body>
 </html>
