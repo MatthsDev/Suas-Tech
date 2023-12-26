c8c8f98d451b4b58e5125c5ba42500ee3223cfad
Diego-m-dev <diegomoreira.pab@gmail.com>
2023-12-26 11:58:31 -0300

    1

--- cras/views/cons_fam.php
+++ cras/views/cons_fam.php
@@ -27,11 +27,18 @@
             <button type="button" onclick="consultarFamilias()">Buscar</button>
         </form>
         <div id="resultado"></div>
+
+
+
+        
         <div class="col-md-6">
             <div class="float-right">
                 <a id="btn-novo" data-toggle="modal" data-target="#modal"></a>
             </div>
         </div>
+
+
+
         <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
             aria-hidden="true">
             <div class="modal-dialog modal-lg" role="document">
