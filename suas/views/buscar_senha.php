2d309a192d0e005a9a329f65d430e0c70a223205
Diego Cadete <engmecanicocadete@gmail.com>
2024-01-24 00:56:37 -0300

    Tela chamar senha

--- suas/views/buscar_senha.php
+++ suas/views/buscar_senha.php
@@ -6,7 +6,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/cadunico/controller/acesso_
 $timestamp = date('Y-m-d H:i:s');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
+    
     $stmt = $pdo->prepare('SELECT * FROM senhas ORDER BY tipo_prioridade ASC, ordem_chegada ASC');
     $stmt->execute();
 
@@ -39,7 +39,94 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             // Execute a nova consulta com os parâmetros necessários
             $stmtUpdate->execute([$situacao_senha, $nome, $timestamp, $setor, $nome_atendido]);
 
-            exit; // Adicionei a instrução exit para encerrar a execução após encontrar uma senha válida.
+            exit; // A instrução exit encerra a execução após encontrar uma senha válida.
+        } else if ($key['tipo_prioridade'] == 'M' && $key['ordem_chegada'] <= 2 && $key['situacao_senha'] == 0){
+            $senha_chamada = $key['ordem_chegada'] . "." . $key['senha'];
+            $nome_atendido = $key['nome_atendido'];
+
+            if ($key['tipo_atendimento'] == "CAD") {
+                $tipo_atendimento = 'CADASTRO ÚNICO';
+            } elseif ($key['tipo_atendimento'] == "PBF") {
+                $tipo_atendimento = 'BOLSA FAMILIA';
+            } elseif ($key['tipo_atendimento'] == "CONC") {
+                $tipo_atendimento = 'CONCESSÃO';
+            } elseif ($key['tipo_atendimento'] == "AMI") {
+                $tipo_atendimento = 'ALISTAMENTO MILITAR';
+            } elseif ($key['tipo_atendimento'] == "RFB") {
+                $tipo_atendimento = 'RECEITA FEDERAL';
+            }
+            $situacao_senha = 1;
+
+            echo json_encode(array('encontrado' => true, 'senha' => $senha_chamada, 'nome' => $nome_atendido, 'atendimento' => $tipo_atendimento));
+
+            // Prepare a nova consulta utilizando a extensão PDO
+            $stmtUpdate = $pdo->prepare("UPDATE senhas SET situacao_senha=?, atendente=?, data_hora_atendido=?, setor_atendido=? WHERE nome_atendido=?");
+            if (!$stmtUpdate) {
+                die('Erro na preparação da consulta: ' . $pdo->errorInfo());
+            }
+
+            // Execute a nova consulta com os parâmetros necessários
+            $stmtUpdate->execute([$situacao_senha, $nome, $timestamp, $setor, $nome_atendido]);
+
+            exit; // A instrução exit encerra a execução após encontrar uma senha válida.
+        } else if ($key['tipo_prioridade'] == 'S' && $key['ordem_chegada'] <= 2 && $key['situacao_senha'] == 0){
+            $senha_chamada = $key['ordem_chegada'] . "." . $key['senha'];
+            $nome_atendido = $key['nome_atendido'];
+
+            if ($key['tipo_atendimento'] == "CAD") {
+                $tipo_atendimento = 'CADASTRO ÚNICO';
+            } elseif ($key['tipo_atendimento'] == "PBF") {
+                $tipo_atendimento = 'BOLSA FAMILIA';
+            } elseif ($key['tipo_atendimento'] == "CONC") {
+                $tipo_atendimento = 'CONCESSÃO';
+            } elseif ($key['tipo_atendimento'] == "AMI") {
+                $tipo_atendimento = 'ALISTAMENTO MILITAR';
+            } elseif ($key['tipo_atendimento'] == "RFB") {
+                $tipo_atendimento = 'RECEITA FEDERAL';
+            }
+            $situacao_senha = 1;
+
+            echo json_encode(array('encontrado' => true, 'senha' => $senha_chamada, 'nome' => $nome_atendido, 'atendimento' => $tipo_atendimento));
+
+            // Prepare a nova consulta utilizando a extensão PDO
+            $stmtUpdate = $pdo->prepare("UPDATE senhas SET situacao_senha=?, atendente=?, data_hora_atendido=?, setor_atendido=? WHERE nome_atendido=?");
+            if (!$stmtUpdate) {
+                die('Erro na preparação da consulta: ' . $pdo->errorInfo());
+            }
+
+            // Execute a nova consulta com os parâmetros necessários
+            $stmtUpdate->execute([$situacao_senha, $nome, $timestamp, $setor, $nome_atendido]);
+
+            exit; // A instrução exit encerra a execução após encontrar uma senha válida.
+        } else if ($key['tipo_prioridade'] == 'B' && $key['ordem_chegada'] <= 2 && $key['situacao_senha'] == 0){
+            $senha_chamada = $key['ordem_chegada'] . "." . $key['senha'];
+            $nome_atendido = $key['nome_atendido'];
+
+            if ($key['tipo_atendimento'] == "CAD") {
+                $tipo_atendimento = 'CADASTRO ÚNICO';
+            } elseif ($key['tipo_atendimento'] == "PBF") {
+                $tipo_atendimento = 'BOLSA FAMILIA';
+            } elseif ($key['tipo_atendimento'] == "CONC") {
+                $tipo_atendimento = 'CONCESSÃO';
+            } elseif ($key['tipo_atendimento'] == "AMI") {
+                $tipo_atendimento = 'ALISTAMENTO MILITAR';
+            } elseif ($key['tipo_atendimento'] == "RFB") {
+                $tipo_atendimento = 'RECEITA FEDERAL';
+            }
+            $situacao_senha = 1;
+
+            echo json_encode(array('encontrado' => true, 'senha' => $senha_chamada, 'nome' => $nome_atendido, 'atendimento' => $tipo_atendimento));
+
+            // Prepare a nova consulta utilizando a extensão PDO
+            $stmtUpdate = $pdo->prepare("UPDATE senhas SET situacao_senha=?, atendente=?, data_hora_atendido=?, setor_atendido=? WHERE nome_atendido=?");
+            if (!$stmtUpdate) {
+                die('Erro na preparação da consulta: ' . $pdo->errorInfo());
+            }
+
+            // Execute a nova consulta com os parâmetros necessários
+            $stmtUpdate->execute([$situacao_senha, $nome, $timestamp, $setor, $nome_atendido]);
+
+            exit; // A instrução exit encerra a execução após encontrar uma senha válida.
         }
     }
 
