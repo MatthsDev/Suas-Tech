6d6fe7d2de8aa0baffb506277cb1eb31dba2ac91
emmanuelcadete <engmecanicocadete@gmail.com>
2024-01-28 23:12:32 -0300

    NOSSA

---
+++ cozinha_comunitaria/js/encerrarEntregas.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $('#gerar_relatorio').click(function () {
+        var confirmarAcao = confirm("Essa ação vai encerrar a entrega. NÃO sendo possível retoma-la hoje, deseja continuar?");
+        var gerar_relatorio = true;
+
+        if (confirmarAcao) {
+
+            $.ajax({
+                type: 'POST',
+                url: '../gerar_relatorio_diario.php',
+                data: {
+                    gerar_relatorio: gerar_relatorio
+                },
+                dataType: 'json',
+                success: function (response) {
+                    if (response.encontrado) {
+                        alert('Relatório diário gerado com sucesso!');
+                    } else {
+                        alert('Erro ao gerar relatório diário!');
+                    }
+
+                }
+            })
+            $('#buscar').hide();
+            $('#gerar_relatorio').hide();
+
+            // Armazenar a hora em que os botões foram escondidos no localStorage
+            localStorage.setItem('horaEsconderBotoes', new Date().getTime());
+
+        } else {
+            alert('Ação cancelada!')
+        }
+    });
+
+    // Verificar se os botões devem ser mostrados ao carregar a página
+    var horaEsconderBotoes = localStorage.getItem('horaEsconderBotoes');
+    if (horaEsconderBotoes) {
+        var tempoPassado = new Date().getTime() - parseInt(horaEsconderBotoes);
+        var horasParaEsconder = 8 * 60 * 60 * 1000;
+
+        // Se o tempo passado for menor que o tempo para esconder os botões, mantenha-os escondidos
+        if (tempoPassado < horasParaEsconder) {
+            $('#buscar').hide();
+            $('#gerar_relatorio').hide();
+            $('#mensagem_bloqueio').text('Após encerrar as entregas do dia e gerar o Relatório o sistema é bloqueado retornando apenas no dia seguinte.');
+        }
+    }
+});
