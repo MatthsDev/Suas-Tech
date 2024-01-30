1c537f6884b131cacd514e7518c52870f6460e4a
Diego-m-dev <diegomoreira.pab@gmail.com>
2024-01-30 14:46:30 -0300

    1

--- cozinha_comunitaria/views/fluxo_diario.php
+++ cozinha_comunitaria/views/fluxo_diario.php
@@ -123,12 +123,8 @@ if (!isset($_GET['nis'])) {
     <p id="mensagem_bloqueio"></p>
     
         <button id='gerar_relatorio' type="submit" name="gerar_relatorio">ENCERRAR AS ENTREGAS</button>
-<<<<<<< HEAD
     </form>
     </div>
-=======
-
->>>>>>> b3808156966fe0ba850b071073d496ecf9c117b5
     <script src='../../controller/back.js'></script>
 </body>
 </html>
