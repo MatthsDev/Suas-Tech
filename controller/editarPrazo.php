3269f820b192a7399d13fbb442c229cefff02e21
emmanuelcadete <engmecanicocadete@gmail.com>
2024-02-04 16:08:50 -0300

    PARECE NO FIM

--- controller/editarPrazo.php
+++ controller/editarPrazo.php
@@ -1,24 +1,32 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/config/conexao.php';
 
-// Verifica se a requisição é do tipo POST
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recebe a data do novo prazo "Y-m-d"
-    $novoPrazo = $_POST['novoPrazo'];
-    $id = $_POST['id'];
+// Verifica se o método da requisição é POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Recebe os IDs enviados na requisição
+    $ids = $_POST['ids'];
 
-    // Solicitação SQL para alterar o prazo no banco de dados
-    $edita_prazo = "UPDATE fluxo_diario_coz SET data_limite = '$novoPrazo' WHERE id = $id";
+    // Solicita a nova data do prazo "Y-m-d" ao usuário
+    $dataOriginal = $_POST['novaData'];
+    $dataFormatando = DateTime::createFromFormat('d/m/Y', $dataOriginal);
+    $novoPrazo = $dataFormatando->format('Y-m-d');
 
-    // Executa a solicitação SQL e verifica se foi bem-sucedida
-    if ($conn->query($edita_prazo) === true) {
-        // Retorna um JSON indicando que a atualização foi bem-sucedida
-        echo json_encode(array('encontrado' => true));
-    } else {
-        // Retorna um JSON indicando que a atualização falhou, incluindo detalhes do erro
-        echo json_encode(array('encontrado' => false, 'erro' => 'Falha na atualização de dados: ' . $conn->error));
+    foreach ($ids as $id) {
+        // Solicitação SQL para alterar o prazo no banco de dados
+        $edita_prazo = "UPDATE fluxo_diario_coz SET data_limite = '$novoPrazo' WHERE id = $id";
+
+        // Executa a solicitação SQL e verifica se foi bem-sucedida
+        if ($conn->query($edita_prazo) === true) {
+            // Retorna um JSON indicando que a atualização foi bem-sucedida
+            echo json_encode(array('encontrado' => true, 'dataNova' => $novoPrazo, 'id_benef' => $id));
+        } else {
+            // Retorna um JSON indicando que a atualização falhou, incluindo detalhes do erro
+            echo json_encode(array('encontrado' => false, 'erro' => 'Falha na atualização de dados: ' . $conn->error));
+        }
     }
 } else {
-    // Retorna um JSON indicando que o método de requisição não é válido (não é POST)
-    echo json_encode(array('erro' => 'Método de requisição inválido!'));
+    // Se a requisição não for POST, emite um erro
+    http_response_code(400);
+    echo json_encode(array('erro' => 'Método de requisição inválido.'));
 }
+?>
