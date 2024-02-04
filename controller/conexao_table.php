6da25a7a694b6b6a760fc29a6835a027c1db8a38
emmanuelcadete <engmecanicocadete@gmail.com>
2024-02-04 11:13:09 -0300

    Editar prazo tela cliente

--- controller/conexao_table.php
+++ controller/conexao_table.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../cadunico/img/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="../cras/css/style-tabela.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+
     <title>Tabela</title>
 </head>
 
@@ -30,13 +34,15 @@ if ($table_fluxo) {
     <form action="processa_exclusao.php" method="post">
         <table width="650px" border="1">
             <tr>
-                <th colspan="5" class="cabecalho"><?php echo "LISTA DAS PESSOAS COM O PRAZO VENCIDO"; ?></th>
+                <th colspan="7" class="cabecalho"><?php echo "LISTA DAS PESSOAS COM DIVERGENCIAS NA COZINHA COMUNITÁRIA"; ?></th>
             </tr>
             <tr>
+                <th>CPF:</th>
                 <th>NOME:</th>
                 <th>DATA CADASTRADO:</th>
                 <th>DATA LIMITE:</th>
-                <th>ENCAMINHADO:</th>
+                <th>ENCAMINHADO</th>
+                <th>MOTIVO</th>
                 <th>AÇÃO</th>
             </tr>
             <?php
@@ -45,10 +51,12 @@ foreach ($dados_table_fluxo as $linhas) {
 
             ?>
                             <tr>
+                                <td> <?php echo $linhas['cpf_benef']; ?> </td>
                                 <td> <?php echo $linhas['nome']; ?> </td>
                                 <td> <?php echo $linhas['data_registro']; ?> </td>
                                 <td> <?php echo $linhas['data_limite']; ?> </td>
                                 <td> <?php echo $linhas['encaminhado_cras']; ?> </td>
+                                <td> FORA DO PRAZO </td>
                                 <td class="check" >
                                 <label class="urg">
                                 <input type="checkbox" name="excluir[]" value="<?php echo $linhas['id']; ?>">
@@ -60,7 +68,26 @@ foreach ($dados_table_fluxo as $linhas) {
 // Define a variável de sessão com base na presença de valores na variável
             $_SESSION['fluxo_diario_coz_has_values'] = $linhas['nome'];
         }
+
+        if ($linhas['limiter'] >= 3) {
+            ?>
+            <tr>
+            <td> <?php echo $linhas['cpf_benef']; ?> </td>
+            <td> <?php echo $linhas['nome']; ?> </td>
+            <td> <?php echo $linhas['data_registro']; ?> </td>
+            <td> <?php echo $linhas['data_limite']; ?> </td>
+            <td> <?php echo $linhas['encaminhado_cras']; ?> </td>
+            <td> <?php echo $linhas['limiter']; ?> FALTAS </td>
+            <td class="check" >
+            <label class="urg">
+            <input type="checkbox" name="excluir[]" value="<?php echo $linhas['id']; ?>">
+            <span class="checkmark"></span>
+            </label>
+            </td>
+        </tr>
+        <?php
     }
+}
     ?>
                 </table>
                 <div class="botoes">
@@ -68,7 +95,7 @@ foreach ($dados_table_fluxo as $linhas) {
                     <!-- Modal -->
                     <div id="modal" class="modal" style="display: none;">
                         <button type="button" onclick="excluirSelecionados()">Excluir</button>
-                        <button type="button" onclick="editarPrazo()">Editar Prazo</button>
+                        <button type="button" onclick="editarPrazo(<?php echo $linhas['id']; ?>)">Editar Prazo</button>
                     </div>
                 </div>
                 <!-- Fim Modal -->
@@ -83,11 +110,34 @@ foreach ($dados_table_fluxo as $linhas) {
                     document.forms[0].submit(); // Envia o formulário
                 }
 
-                function editarPrazo() {
-                    // Lógica para editar o prazo
-                    // Pode abrir outro modal, redirecionar para uma página de edição, etc.
-                    alert('Implemente a lógica para editar o prazo aqui.');
-                }
+                function editarPrazo(id) {
+    var novoPrazo = window.prompt('Informe o novo prazo para esta família (Formato: DD/MM/AAAA)');
+
+    // Utiliza o moment.js para formatar a data
+    var dataFormatada = moment(novoPrazo, 'DD/MM/YYYY').format('YYYY-MM-DD');
+
+    $.ajax({
+        url: 'editarPrazo.php',
+        method: 'POST',
+        data: {
+            id: id,
+            novoPrazo: dataFormatada
+        },
+        dataType: 'json',
+        success: function(response) {
+            if (response.encontrado) {
+                alert('Data alterada com sucesso!');
+                location.reload();
+            } else {
+                alert('Erro ao salvar a data! Consulte o SUPORTE DDV.');
+            }
+        },
+        error: function(error) {
+            console.error('Erro ao enviar dados: ' + error);
+        }
+    });
+}
+
             </script>
         <?php
 } else {
@@ -95,7 +145,7 @@ foreach ($dados_table_fluxo as $linhas) {
 }
 
 if (empty($dados_table_fluxo)) {
-    echo "<tr><td colspan='6'>Nenhuma família fora do prazo.</td></tr>";
+    echo "<tr><td colspan='7'>Nenhuma família fora do prazo.</td></tr>";
 }
 ?>
 </div>
