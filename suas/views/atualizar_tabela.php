8734265274972c0951d430024dfd57611d677b72
Diego-m-dev <diegomoreira.pab@gmail.com>
2024-02-06 11:59:35 -0300

    2

--- suas/views/atualizar_tabela.php
+++ suas/views/atualizar_tabela.php
@@ -16,33 +16,31 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/config/sessao.php';
 </head>
 
 <body>
+    <div class="img">
+        <h1 class="titulo-com-imagem">
+            <img class="titulo-com-imagem" src="../img/h1-banco.svg" alt="Titulocomimagem">
+        </h1>
+    </div>
+    <div class="container">
+        <form action="../controller/import.php" method="POST" enctype="multipart/form-data">
 
-    <body>
-        <div class="img">
-            <h1 class="titulo-com-imagem">
-                <img class="titulo-com-imagem" src="../img/h1-banco.svg" alt="Titulocomimagem">
-            </h1>
-        </div>
-        <div class="container">
-            <form action="../controller/import.php" method="POST" enctype="multipart/form-data">
+            <label>Qual tabela você pretende atualizar: </label>
+            <select name="csv_tbl" required>
+                <option value="tudo">Base de Dados do Cadastro Único</option>
+                <option value="folha">Folha de Pagamento</option>
 
-                <label>Qual tabela você pretende atualizar: </label>
-                <select name="csv_tbl" required>
-                    <option value="tudo">Base de Dados do Cadastro Único</option>
-                    <option value="folha">Folha de Pagamento</option>
+            </select>
 
-                </select>
-
-                Selecione o arquivo CSV: <input type="file" name="arquivoCSV" id="arquivoCSV" accept=".csv">
-                <button type="submit" value="Importar"> Importar </button>
-                <div class="btn">
-                    <a onclick="goBack()">
-                        <i class="fas fa-arrow-left"></i> Voltar ao menu
-                    </a>
-                </div>
-            </form>
-        </div>
-    </body>
-    <script src='../../controller/back.js'></script>
+            Selecione o arquivo CSV: <input type="file" name="arquivoCSV" id="arquivoCSV" accept=".csv">
+            <button type="submit" value="Importar"> Importar </button>
+            <div class="btn">
+                <a onclick="goBack()">
+                    <i class="fas fa-arrow-left"></i> Voltar ao menu
+                </a>
+            </div>
+        </form>
+    </div>
+</body>
+<script src='../../controller/back.js'></script>
 
 </html>
