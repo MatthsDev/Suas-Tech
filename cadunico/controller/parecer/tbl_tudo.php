db8176a55eade2fa6da924dcfba318d1ae2ceee6
emmanuelcadete <engmecanicocadete@gmail.com>
2024-02-06 16:44:19 -0300

    treza

--- cadunico/controller/parecer/tbl_tudo.php
+++ cadunico/controller/parecer/tbl_tudo.php
@@ -3,7 +3,8 @@
 include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/config/conexao.php';
 ?>
 
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <table width="650px" border="1">
 
 <tr class="titulo" >
@@ -15,7 +16,12 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/config/conexao.php';
     <th class="cabecalho">DATA ATUALIZAÇÃO</th>
     <th class="cabecalho">DATA DE NASCIMENTO</th>
     <th class="cabecalho">ENDEREÇO</th>
-
+    <th class="check">
+                <label class="urg">
+                        <input type="checkbox" id="selecionarTodos">
+                    <span class="checkmark"></span>
+                </label>
+            </th>
 </tr>
 
     <?php
@@ -23,18 +29,18 @@ if (!isset($_GET['ano_select'])) {
 } else {
     $sql_cod = $conn->real_escape_string($_GET['ano_select']);
     $sqli_cod = $conn->real_escape_string($_GET['localidade']);
-    $sql_dados = "SELECT * FROM tbl_tudo WHERE dat_atual_fam LIKE '%$sql_cod%' AND nom_localidade_fam LIKE '%$sqli_cod%'";
+    $sql_dados = "SELECT * FROM tbl_tudo WHERE dat_atual_fam LIKE '%$sql_cod%' AND nom_localidade_fam LIKE '%$sqli_cod%' AND cod_parentesco_rf_pessoa = 1";
     $sql_query = $conn->query($sql_dados) or die("ERRO ao consultar !" . $conn - error);
 
     if ($sql_query->num_rows == 0) {
         ?>
-<tr class="resultado">
-<td colspan="5">Nenhum resultado encontrado...</td>
-</tr>
+    <tr class="resultado">
+    <td colspan="7">Nenhum resultado encontrado...</td>
+    </tr>
         <?php
 } else {
 
-                    while ($dados = $sql_query->fetch_assoc()) {
+        while ($dados = $sql_query->fetch_assoc()) {
             ?>
         <tr class="resultado">
             <td class="resultado"><?php echo $dados['cod_familiar_fam']; ?></td>
@@ -43,36 +49,53 @@ if (!isset($_GET['ano_select'])) {
             <td class="resultado"><?php echo $dados['nom_completo_mae_pessoa']; ?></td>
             <td class="resultado"><?php echo $dados['dat_atual_fam']; ?></td>
             <td class="resultado"><?php echo $dados['dta_nasc_pessoa']; ?></td>
-            <td class="resultado"><?php 
-                                //construindo o endereço
-                                $tipo_logradouro = $dados["nom_tip_logradouro_fam"];
-                                $nom_logradouro_fam = $dados["nom_logradouro_fam"];
-                                $num_logradouro_fam = $dados["num_logradouro_fam"];
-                                if ($num_logradouro_fam == "") {
-                                    $num_logradouro = "S/N";
-                                } else {
-                                    $num_logradouro = $dados["num_logradouro_fam"];
-                                }
-                                $nom_localidade_fam = $dados["nom_localidade_fam"];
-                                $nom_titulo_logradouro_fam = $dados["nom_titulo_logradouro_fam"];
-                                if ($nom_titulo_logradouro_fam == "") {
-                                    $nom_tit = "";
-                                } else {
-                                    $nom_tit = $dados["nom_titulo_logradouro_fam"];
-                                }
-                                $txt_referencia_local_fam = $dados["txt_referencia_local_fam"];
-                                if ($txt_referencia_local_fam == "") {
-                                    $referencia = "SEM REFERÊNCIA";
-                                } else {
-                                    $referencia = $dados["txt_referencia_local_fam"];
-                                }
-            
+            <td class="resultado"><?php
+//construindo o endereço
+            $tipo_logradouro = $dados["nom_tip_logradouro_fam"];
+            $nom_logradouro_fam = $dados["nom_logradouro_fam"];
+            $num_logradouro_fam = $dados["num_logradouro_fam"];
+            if ($num_logradouro_fam == "") {
+                $num_logradouro = "S/N";
+            } else {
+                $num_logradouro = $dados["num_logradouro_fam"];
+            }
+            $nom_localidade_fam = $dados["nom_localidade_fam"];
+            $nom_titulo_logradouro_fam = $dados["nom_titulo_logradouro_fam"];
+            if ($nom_titulo_logradouro_fam == "") {
+                $nom_tit = "";
+            } else {
+                $nom_tit = $dados["nom_titulo_logradouro_fam"];
+            }
+            $txt_referencia_local_fam = $dados["txt_referencia_local_fam"];
+            if ($txt_referencia_local_fam == "") {
+                $referencia = "SEM REFERÊNCIA";
+            } else {
+                $referencia = $dados["txt_referencia_local_fam"];
+            }
+
             $endereco_conpleto = $tipo_logradouro . " " . $nom_tit . " " . $nom_logradouro_fam . ", " . $num_logradouro . " - " . $nom_localidade_fam . ", " . $referencia;
-            
-            echo $endereco_conpleto; ?></td>
+
+            echo $endereco_conpleto;?></td>
+            <td class="check">
+                <label class="urg">
+                        <input type="checkbox" name="excluir[]" value="<?php echo $dados['num_nis_pessoa_atual']; ?>">
+                    <span class="checkmark"></span>
+                </label>
+            </td>
         </tr>
 <?php
 }
     }
 }?>
 </table>
+
+    <script>
+        document.getElementById('selecionarTodos').addEventListener('click', function (){
+            // Obter todos os checkboxes na tabela
+            var checkBoxes = document.querySelectorAll('input[name="excluir[]"]')
+
+            checkBoxes.forEach(function(checkbox){
+                checkbox.checked = document.getElementById('selecionarTodos').checked
+            })
+        })
+    </script>
