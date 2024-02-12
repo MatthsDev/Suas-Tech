c218b836f2da44b33d174d9cdae5eb3d4091a7fd
emmanuelcadete <engmecanicocadete@gmail.com>
2024-02-12 17:38:10 -0300

    Linha duplicada sem necessidade

--- cadunico/views/visit/visitas_para_fazer.php
+++ cadunico/views/visit/visitas_para_fazer.php
@@ -25,7 +25,6 @@ $data9 = $data1 - 1;
     <link rel="website icon" type="png" href="../../img/logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <body>
     <div class="img">
