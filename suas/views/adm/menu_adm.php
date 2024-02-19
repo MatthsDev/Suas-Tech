935a6290ab187d6abde128d73e7dfe1ab523fa43
Diego-m-dev <diegomoreira.pab@gmail.com>
2024-02-19 17:06:58 -0300

    adm

--- suas/views/adm/menu_adm.php
+++ suas/views/adm/menu_adm.php
@@ -8,13 +8,42 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/cadunico/controller/acesso_
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="/Suas-Tech/img/logo.png" type="image/x-icon">
-    <title>Administrativo</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <title>TechSUAS - Administrativo</title>
+    <link rel="stylesheet" href="css/style_menu.css">
 </head>
 <body>
-
-<button type="button" id="cadastrar_contrato" onclick="window.location.href='/Suas-Tech/suas/views/adm/cadastro_contrato.php'">Cadastrar Contrato</button>
-<button type="button" id="consulta_contrato" onclick="window.location.href='/Suas-Tech/suas/views/adm/contratos.php'">Contratos</button>
-
+<div class="img">
+        <h1 class="titulo-com-imagem">
+            <img class="titulo-com-imagem" src="../adm/img/h1-adm.svg" alt="Titulocomimagem">
+        </h1>
+</div>
+<div class="apelido">    
+        <h3>Bem-vindo (a)
+            <?php echo $apelido; ?>.
+        </h3> 
+    </div> 
+        <div class="container">
+            <nav>
+                <div class="btn">
+                    <a class="menu-button" id="cadastrar_contrato" onclick="window.location.href='/Suas-Tech/suas/views/adm/cadastro_contrato.php'">
+                        <span class="material-symbols-outlined">
+                        contract_edit
+                        </span>
+                        Cadastrar Contratos
+                    </a>
+                </div>
+                <div class="btn">
+                    <a class="menu-button" id="cadastrar_contrato" onclick="window.location.href='/Suas-Tech/suas/views/adm/contratos.php'">
+                        <span class="material-symbols-outlined">
+                        contract
+                        </span>
+                        Consultar Contratos
+                    </a>
+                </div>
+            </nav>
+        </div>
+<footer><img src="img/footer-adm.svg" alt=""></footer>
 </body>
 </html>
 
