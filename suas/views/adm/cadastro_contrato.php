eb79a2a214f56ae267decda615c8b3377131acef
Diego-m-dev <diegomoreira.pab@gmail.com>
2024-02-21 12:40:42 -0300

    1

--- suas/views/adm/cadastro_contrato.php
+++ suas/views/adm/cadastro_contrato.php
@@ -4,6 +4,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/cadunico/controller/acesso_
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,90 +14,112 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/cadunico/controller/acesso_
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+
     <title>Cadastrar Contrato</title>
 </head>
+
 <body>
-<div class="img">
+    <div class="img">
         <h1 class="titulo-com-imagem">
             <img class="titulo-com-imagem" src="../adm/img/cadas_cont.svg" alt="Titulocomimagem">
         </h1>
-</div>
-<div class="container">
-    <div id='form_contrato'>
-    <form action="/Suas-Tech/suas/controller/salvar_contrato.php" method="POST">
-            <!-- Campos do Contrato -->
-            <div>
-                <label>Número contrato:</label>
-                <input type="text" name="num_contrato">
-            </div>
-            <div>
-                <label>Nome empresa:</label>
-                <input type="text" name="nome_empresa" class="inpu">
-            </div>
-            <div>
-                <label>Razão social:</label>
-                <input type="text" name="razao_social" class="inpu">
-            </div>
-            <div>
-                <label>CNPJ:</label>
-                <input type="text" name="cnpj" id="cnpjInput" maxlength="18">
-            </div>
-            <div>
-                <label>Contato:</label>
-                <input type="text" name="num_contato" id="contatoInput" maxlength="16">
-            </div>
-            <div>
-                <label>Data Assinatura:</label>
-                <input type="date" name="data_assinatura">
-            </div>
-            <div>
-                <label>Prazo Vigente:</label>
-                <input type="date" name="vigencia">
-            </div>
-        <div class="btns">
-            <button type="submit" name="btn_contrato">Salvar Contrato</button>
-            <button id='btn_salva_contrato'>Cadastrar Itens</button>
+    </div>
+    <div class="container">
+        <div id='form_contrato'>
+            <form action="/Suas-Tech/suas/controller/salvar_contrato.php" method="POST">
+                <!-- Campos do Contrato -->
+                <div>
+                    <label>Número contrato:</label>
+                    <input type="text" name="num_contrato">
+                </div>
+                <div>
+                    <label>Nome empresa:</label>
+                    <input type="text" name="nome_empresa" class="inpu">
+                </div>
+                <div>
+                    <label>Razão social:</label>
+                    <input type="text" name="razao_social" class="inpu">
+                </div>
+                <div>
+                    <label>CNPJ:</label>
+                    <input type="text" name="cnpj" id="cnpjInput" maxlength="18">
+                </div>
+                <div>
+                    <label>Telefone:</label>
+                    <input type="text" name="num_contato" id="contatoInput" maxlength="16">
+                </div>
+                <div>
+                    <label>E-mail:</label>
+                    <input type="text" name="#" class="inpu">
+                </div>
+                <div>
+                    <label for="valor_contrato">Valor do contrato:</label>
+                    <span>R$</span>
+                    <input type="text" name="valor" id="valor_contrato" />
+                </div>
+                <div>
+                    <label>Data Assinatura:</label>
+                    <input type="date" name="data_assinatura">
+                </div>
+                <div>
+                    <label>Prazo Vigente:</label>
+                    <input type="date" name="vigencia">
+                </div>
+                <div class="btns">
+                    <button type="submit" name="btn_contrato">Salvar Contrato</button>
+                    <button id='btn_salva_contrato'>Cadastrar Itens</button>
+                </div>
+            </form>
         </div>
-    </form>
-    </div>
-    <div class="back">             
-        <button type="button" onclick="window.location.href ='/Suas-Tech/suas/views/adm/menu_adm.php';">
-            <i class="fas fa-arrow-left"></i>
-            Voltar ao menu
-        </button>
-    </div>
-    <div id='form_itens' style='display: none'>
-    <form action="/Suas-Tech/suas/controller/salvar_contrato.php" method="POST">
-            <!-- Tabela de Itens -->
-            <label>Informe o número contrato:</label>
-            <input type=text name="num_contrato" placeholder="Os itens pertence a qual contrato?">
-            <table width="500px" border="1" id="tabelaItens">
-                <tr>
-                    <th colspan="5">TABELA ITENS</th>
-                </tr>
-                <tr>
-                    <th>Código do Item</th>
-                    <th>Nome</th>
-                    <th>Quantidade</th>
-                    <th>Valor Unitário</th>
-                    <th>Valor Total</th>
-                </tr>
-                <tr>
-                    <td><input type="text" name="num_item[]"></td>
-                    <td><input type="text" name="nome_produto[]"></td>
-                    <td><input type="text" name="quantidade[]" class="quantidade"></td>
-                    <td><input type="text" name="valor_unitario[]" class="valor-unitario"></td>
-                    <td><input type="text" name="valor_total[]" class="valor-total" readonly></td>
-                </tr>
-            </table>
+        <div class="back">
+            <button type="button" onclick="window.location.href ='/Suas-Tech/suas/views/adm/menu_adm.php';">
+                <i class="fas fa-arrow-left"></i>
+                Voltar ao menu
+            </button>
+        </div>
+        <div id='form_itens' style='display: none'>
+            <form action="/Suas-Tech/suas/controller/salvar_contrato.php" method="POST">
+                <!-- Tabela de Itens -->
+                <label>Informe o número contrato:</label>
+                <input type=text name="num_contrato" placeholder="Os itens pertence a qual contrato?">
+                <table width="500px" border="1" id="tabelaItens">
+                    <tr>
+                        <th colspan="5">TABELA ITENS</th>
+                    </tr>
+                    <tr>
+                        <th>Código do Item</th>
+                        <th>Nome</th>
+                        <th>Quantidade</th>
+                        <th>Valor Unitário</th>
+                        <th>Valor Total</th>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name="num_item[]"></td>
+                        <td><input type="text" name="nome_produto[]"></td>
+                        <td><input type="text" name="quantidade[]" class="quantidade"></td>
+                        <td><input type="text" name="valor_unitario[]" class="valor-unitario"></td>
+                        <td><input type="text" name="valor_total[]" class="valor-total" readonly></td>
+                    </tr>
+                </table>
 
-            <!-- Botões -->
-            <button type="button" onclick="adicionarLinha()">Adicionar Linha</button>
-        <button type="submit" name="btn_itens">Salvar Itens</button>
-    </form>
-    </div>
+                <!-- Botões -->
+                <button type="button" onclick="adicionarLinha()">Adicionar Linha</button>
+                <button type="submit" name="btn_itens">Salvar Itens</button>
+            </form>
+        </div>
 
-</div>
+    </div>
     <script src="/Suas-Tech/suas/js/script_contrato.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#valor_contrato').mask('000.000.000.000.000,00', {
+                reverse: true
+            });
+        });
+    </script>
+
 </body>
+
 </html>
--- suas/views/adm/contratos.php
+++ suas/views/adm/contratos.php
@@ -10,145 +10,162 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/cadunico/controller/acesso_
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="/Suas-Tech/img/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/style_cad_cont.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <title>Cadastrar Contrato</title>
 </head>
 <body>
-<!-- Formulário de busca -->
-<form method="">
-<label>Buscar contrato:</label>
-<input type="text" name="buscar" placeholder="Buscar por qualquer informação do contrato..." required>
-<p id="info_paragrafo">caso sua busca seja feita por cnpj lembre-se de colocar os caracter (. / -) para a busca retornar com sucesso.</p>
-<button id="btn_busca">BUSCAR</button>
-</form>
+<div class="img">
+        <h1 class="titulo-com-imagem">
+            <img class="titulo-com-imagem" src="../adm/img/consul_cont.svg" alt="Titulocomimagem">
+        </h1>
+</div>
+<div class="container">
+    <!-- Formulário de busca -->
+    <form method="">
+    <label>Buscar contrato:</label>
+    <input type="text" name="buscar" class="inpu1" placeholder="Buscar por qualquer informação do contrato..." required>
+    <p id="info_paragrafo">caso sua busca seja feita por cnpj lembre-se de colocar os caracteres (. / -) para a busca retornar com sucesso.</p>
+    <button id="btn_busca">Buscar</button>
 
-<?php
-// Verifica se o parâmetro 'buscar' não está definido na URL
-if (!isset($_GET['buscar'])) {
+    </form>
 
-} else {
-    // Obtém o valor do parâmetro 'buscar' e evita injeção de SQL
-    $contrato = $conn->real_escape_string($_GET['buscar']);
-    // Consulta o banco de dados com base no contrato informado
-    $valor_contrato = "SELECT * FROM tabela_contrato WHERE num_contrato LIKE '$contrato' OR nome_empresa LIKE '$contrato' OR cnpj LIKE '$contrato'";
-    $contrato_query = $conn->query($valor_contrato) or die("ERRO ao consultar!" . $conn - error);
+    <?php
+    // Verifica se o parâmetro 'buscar' não está definido na URL
+    if (!isset($_GET['buscar'])) {
 
-    // Verifica se nenhum contrato foi encontrado
-    if ($contrato_query->num_rows == 0) {
-        ?>
-        <script>
-            Swal.fire({
-            icon: "error",
-            title: "NÃO ENCONTRADO",
-            text: "Não existe nenhum contrato com essa informação!",
-            confirmButtonText: 'OK',
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    window.location.href = "/Suas-Tech/suas/views/adm/contratos.php";
-                }
-            });
-        </script>
-        <?php
     } else {
-        // Exibe informações do contrato encontrado
-        $dados_contrato = $contrato_query->fetch_assoc();
-        $id_contrato = $dados_contrato['id_contrato'];
+        // Obtém o valor do parâmetro 'buscar' e evita injeção de SQL
+        $contrato = $conn->real_escape_string($_GET['buscar']);
+        // Consulta o banco de dados com base no contrato informado
+        $valor_contrato = "SELECT * FROM tabela_contrato WHERE num_contrato LIKE '$contrato' OR nome_empresa LIKE '$contrato' OR cnpj LIKE '$contrato'";
+        $contrato_query = $conn->query($valor_contrato) or die("ERRO ao consultar!" . $conn - error);
 
-        echo 'Empresa: ' . $dados_contrato['nome_empresa'];
-        echo 'Razão Social: ' . $dados_contrato['razao_social'];
-        echo "CNPJ: " . $dados_contrato['cnpj'];
-        echo "Contato: " . $dados_contrato['num_contato'];
-        echo "Número: " . $dados_contrato['num_contrato'];
-        // cria uma sessão para transmitir o valor para outro php
-            $_SESSION['num_contrato'] = $dados_contrato['num_contrato'];
-
-        // Consulta itens relacionados ao contrato
-        $itens = $conn->real_escape_string($id_contrato);
-        $valor_itens = "SELECT * FROM tabela_itens WHERE id_contrato LIKE '$itens'";
-        $itens_query = $conn->query($valor_itens) or die("ERRO ao consultar!" . $conn - error);
-
-        // Verifica se existem itens relacionados ao contrato
-        if ($itens_query->num_rows == 0) {
-            echo 'Não há itens para esse contrato.';
+        // Verifica se nenhum contrato foi encontrado
+        if ($contrato_query->num_rows == 0) {
+            ?>
+            <script>
+                Swal.fire({
+                icon: "error",
+                title: "NÃO ENCONTRADO",
+                text: "Não existe nenhum contrato com essa informação!",
+                confirmButtonText: 'OK',
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = "/Suas-Tech/suas/views/adm/contratos.php";
+                    }
+                });
+            </script>
+            <?php
         } else {
-            $soma_vlr_tot = 0;
-            ?>
-            <!-- Formulário para editar prazo -->
-            <div id="form_ed_data">
-            <form method="POST" action="/Suas-Tech/suas/controller/editar_prazo.php">
-            <label>Nova data</label>
-            <input type="date" name="data_alt">
-            <label>Apotilamento</label>
-            <input type="text" name="apostilamento_alt">
-            <button type="submit">SALVAR</button>
-        </form>
-        </div>
-        <button id="mostrar_form">Editar dados</button>
-            <!-- Tabela de itens do contrato -->
-            <table width="650px" border="1">
-                <tr>
-                    <th>Código do Item</th>
-                    <th>NOME</th>
-                    <th>Quantidade</th>
-                    <th>Valor Unitário</th>
-                    <th>Total Itens</th>
-                </tr>
-                <?php
-                
-                // Loop para exibir os itens
-                while ($dados_itens = $itens_query->fetch_assoc()) {
+            // Exibe informações do contrato encontrado
+            $dados_contrato = $contrato_query->fetch_assoc();
+            $id_contrato = $dados_contrato['id_contrato'];
+
+            echo 'Empresa: ' . $dados_contrato['nome_empresa'];
+            echo 'Razão Social: ' . $dados_contrato['razao_social'];
+            echo "CNPJ: " . $dados_contrato['cnpj'];
+            echo "Contato: " . $dados_contrato['num_contato'];
+            echo "Número: " . $dados_contrato['num_contrato'];
+            // cria uma sessão para transmitir o valor para outro php
+                $_SESSION['num_contrato'] = $dados_contrato['num_contrato'];
+
+            // Consulta itens relacionados ao contrato
+            $itens = $conn->real_escape_string($id_contrato);
+            $valor_itens = "SELECT * FROM tabela_itens WHERE id_contrato LIKE '$itens'";
+            $itens_query = $conn->query($valor_itens) or die("ERRO ao consultar!" . $conn - error);
+
+            // Verifica se existem itens relacionados ao contrato
+            if ($itens_query->num_rows == 0) {
+                echo 'Não há itens para esse contrato.';
+            } else {
+                $soma_vlr_tot = 0;
                 ?>
-                <tr>
-                    <td><?php echo $dados_itens['num_item']; ?></td>
-                    <td><?php echo $dados_itens['nome_produto']; ?></td>
-                    <td><?php echo $dados_itens['quantidade']; ?></td>
-                    <td><?php
-                        // Formata o valor unitário
-                        $vlr_uni = $dados_itens['valor_unitario'];
-                        $vlr_uni = str_replace(',', '.', str_replace('.', '', $vlr_uni));
-                        $vlr_uni_formatado = 'R$ ' . number_format((float) $vlr_uni / 100, 2, ',', '.');
-                        echo $vlr_uni_formatado;?></td>
-                    <td><?php
-                        // Formata o valor total
-                        $vlr_tot = $dados_itens['valor_total'];
-                        $vlr_tot = str_replace(',', '.', str_replace('.', '', $vlr_tot));
-                        $vlr_tot_formatado = 'R$ ' . number_format((float) $vlr_tot / 100, 2, ',', '.');
-                        echo $vlr_tot_formatado;
-                        
-                        // Acumula o valor na variável de soma
-                        $soma_vlr_tot += (float) $vlr_tot / 100;
+                <!-- Formulário para editar prazo -->
+                <div id="form_ed_data">
+                <form method="POST" action="/Suas-Tech/suas/controller/editar_prazo.php">
+                <label>Nova data</label>
+                <input type="date" name="data_alt">
+                <label>Apotilamento</label>
+                <input type="text" name="apostilamento_alt">
+                <button type="submit">SALVAR</button>
+            </form>
+            </div>
+            <button id="mostrar_form">Editar dados</button>
+                <!-- Tabela de itens do contrato -->
+                <table width="650px" border="1">
+                    <tr>
+                        <th>Código do Item</th>
+                        <th>NOME</th>
+                        <th>Quantidade</th>
+                        <th>Valor Unitário</th>
+                        <th>Total Itens</th>
+                    </tr>
+                    <?php
+                    
+                    // Loop para exibir os itens
+                    while ($dados_itens = $itens_query->fetch_assoc()) {
                     ?>
-                </td>
-                </tr>
-                <?php
-            }
-            ?>
-                <tr>
-                    <td>Total</td>
-                    <td colspan="4">
-                        <?php
-                        // Exibe o total da soma dos valores
-                        echo "R$ " . number_format($soma_vlr_tot, 2, ',', '.');
+                    <tr>
+                        <td><?php echo $dados_itens['num_item']; ?></td>
+                        <td><?php echo $dados_itens['nome_produto']; ?></td>
+                        <td><?php echo $dados_itens['quantidade']; ?></td>
+                        <td><?php
+                            // Formata o valor unitário
+                            $vlr_uni = $dados_itens['valor_unitario'];
+                            $vlr_uni = str_replace(',', '.', str_replace('.', '', $vlr_uni));
+                            $vlr_uni_formatado = 'R$ ' . number_format((float) $vlr_uni / 100, 2, ',', '.');
+                            echo $vlr_uni_formatado;?></td>
+                        <td><?php
+                            // Formata o valor total
+                            $vlr_tot = $dados_itens['valor_total'];
+                            $vlr_tot = str_replace(',', '.', str_replace('.', '', $vlr_tot));
+                            $vlr_tot_formatado = 'R$ ' . number_format((float) $vlr_tot / 100, 2, ',', '.');
+                            echo $vlr_tot_formatado;
+                            
+                            // Acumula o valor na variável de soma
+                            $soma_vlr_tot += (float) $vlr_tot / 100;
                         ?>
                     </td>
-                </tr>
-            </table>
-            <?php
+                    </tr>
+                    <?php
+                }
+                ?>
+                    <tr>
+                        <td>Total</td>
+                        <td colspan="4">
+                            <?php
+                            // Exibe o total da soma dos valores
+                            echo "R$ " . number_format($soma_vlr_tot, 2, ',', '.');
+                            ?>
+                        </td>
+                    </tr>
+                </table>
+                <?php
+            }
         }
     }
-}
-?>
-<script>
-    $(document).ready(function(){
-        $('#form_ed_data').hide()
-    })
-    $('#mostrar_form').click(function(){
-        $('#mostrar_form').hide()
-        $('#form_ed_data').show()
-    })
-</script>
+    ?>
+    <script>
+        $(document).ready(function(){
+            $('#form_ed_data').hide()
+        })
+        $('#mostrar_form').click(function(){
+            $('#mostrar_form').hide()
+            $('#form_ed_data').show()
+        })
+    </script>
+    <div class="back">             
+        <button type="button" onclick="window.location.href ='/Suas-Tech/suas/views/adm/menu_adm.php';">
+            <i class="fas fa-arrow-left"></i>
+            Voltar ao menu
+        </button>
+    </div>
+</div>
+
 </body>
     
 </html>
