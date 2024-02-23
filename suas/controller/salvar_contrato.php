527de4d84e31ff906abb15c1fa067ad9566acd78
emmanuelcadete <engmecanicocadete@gmail.com>
2024-02-23 12:29:29 -0300

    MASTER

--- suas/controller/salvar_contrato.php
+++ suas/controller/salvar_contrato.php
@@ -11,7 +11,6 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/cadunico/controller/acesso_
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 </head>
 <body>
 
