c20594a475e5bab04161f956171b7697f37505ad
emmanuelcadete <engmecanicocadete@gmail.com>
2024-02-25 22:12:33 -0300

    exclur item

---
+++ concessao/controller/excluir_item.php
@@ -0,0 +1,39 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/config/conexao.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Verifica se o array 'excluir' foi enviado
+    if (isset($_POST['excluir']) && is_array($_POST['excluir'])) {
+
+        // Inicializa uma string para armazenar as mensagens de exclusão
+        $mensagensExclusao = "";
+
+        // Loop através dos IDs a serem excluídos
+        foreach ($_POST['excluir'] as $id) {
+            // Declarações preparadas para evitar SQL injection
+            $sql = "DELETE FROM concessao_itens WHERE id_itens_conc = :id";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+            // Executa a exclusão
+            if ($stmt->execute()) {
+                $mensagensExclusao .= "Registro com ID $id excluído com sucesso. <br>";
+            } else {
+                $mensagensExclusao .= "Erro ao excluir o registro com ID $id. <br>";
+            }
+
+        }
+        echo "<script>
+                alert('$mensagensExclusao');
+                window.location.href = '/Suas-Tech/concessao/index.php';
+            </script>";
+
+        } else {
+            echo "Nenhum registro selecionado para exclusão. <br>";
+        }
+} else {
+    echo "Acesso inválido ao script de exclusão. <br>";
+}
+
+// Fechar conexão
+$conn->close();
