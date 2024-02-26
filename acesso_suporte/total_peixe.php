8c4cb219fc285e03261d8f4ba641074d7222942a
emmanuelcadete <engmecanicocadete@gmail.com>
2024-02-26 15:43:02 -0300

    sdfsgsdfg

--- acesso_suporte/total_peixe.php
+++ acesso_suporte/total_peixe.php
@@ -3,16 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/visitas.css">
+    <link rel="stylesheet" href="../cadunico/css/visitas.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="website icon" type="png" href="../../img/logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-    <script src="../../js/scriptTelaChamada.js"></script>
-    <script src="script_chamar.js"></script>
     <title>Tela de Chamadas</title>
 </head>
 <body>
@@ -39,9 +38,9 @@ $sql_ano = 'SELECT local_cadastro AS lc_cad, COUNT(*) AS quantidade
 FROM peixe
 GROUP BY local_cadastro
 ORDER BY lc_cad DESC';
-$resultado_p_ano = $conn->query($sql_ano);
+$resultado_p_local = $conn->query($sql_ano);
 
-while ($linha = $resultado_p_ano->fetch_assoc()) {
+while ($linha = $resultado_p_local->fetch_assoc()) {
     $local_cadastro[] = $linha['lc_cad'];
     $quantidades[] = $linha['quantidade'];
 
@@ -53,15 +52,15 @@ while ($linha = $resultado_p_ano->fetch_assoc()) {
     var anos = <?php echo json_encode($local_cadastro); ?>;
     var quantidades = <?php echo json_encode($quantidades); ?>;
 
-    // Adiciona volaroe às quantidades
-    quantidades.push(volaroe);
+    console.log(anos);
+    console.log(quantidades);
 
     // Crie o gráfico de pizza
     var ctx = document.getElementById('graficoPizza').getContext('2d');
     var myPieChart = new Chart(ctx, {
         type: 'pie',
         data: {
-            labels: anos.concat(['Visitas Feitas']),
+            labels: anos.concat(['Cadastros Realizados']),
             datasets: [{
                 data: quantidades,
                 backgroundColor: ['#13294b', '#065f33', '#8b0000', '#cd5c5c', '#008080', '#2e8b57', '#00ff00', '#ff9900']
