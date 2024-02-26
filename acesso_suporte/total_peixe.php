40bdd7b96f4612ba454c72db5ed25712af9780cb
emmanuelcadete <engmecanicocadete@gmail.com>
2024-02-26 14:36:39 -0300

    peixe

--- acesso_suporte/total_peixe.php
+++ acesso_suporte/total_peixe.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="teste_atende/style_atend.css" />
+    <link rel="shortcut icon" href="../../cadunico/img/logo.png" type="image/x-icon">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script src="../../js/scriptTelaChamada.js"></script>
+    <script src="script_chamar.js"></script>
+    <title>Tela de Chamadas</title>
+</head>
+<body>
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/config/conexao.php';
 //dados totais dos registros de visitas
@@ -8,3 +24,6 @@ $totalRegistros = $row['total_peixe'];
 $numero_parecer = $totalRegistros;
 
 echo $totalRegistros;
+?>
+</body>
+</html>
