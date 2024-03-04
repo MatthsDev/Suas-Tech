b9941c6f43b0fe6f9f22ffd119377380ae066786
emmanuelcadete <engmecanicocadete@gmail.com>
2024-03-04 20:54:27 -0300

    paradise

---
+++ concessao/js/personalise.js
@@ -0,0 +1,7 @@
+
+function iniciarEdicao(campo) {
+    // Oculta a visualização e exibe o campo de edição
+    document.getElementById(campo + 'Visual').style.display = 'none';
+    document.getElementById(campo + 'Edicao').style.display = 'block';
+}
+
