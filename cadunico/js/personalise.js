ec138e78b1a7837720d0512d5615ac73fad2de38
emmanuelcadete <engmecanicocadete@gmail.com>
2024-03-04 08:45:32 -0300

    personalise

--- cadunico/js/personalise.js
+++ cadunico/js/personalise.js
@@ -9,3 +9,41 @@ function sempre_maiusculo(elemento) {
         document.getElementById(campo + 'Edicao').style.display = 'block';
     }
     
+
+
+    document.getElementById('selecionarTodos').addEventListener('click', function (){
+        // Obter todos os checkboxes na tabela
+        var checkBoxes = document.querySelectorAll('input[name="excluir[]"]')
+
+        checkBoxes.forEach(function(checkbox){
+            checkbox.checked = document.getElementById('selecionarTodos').checked
+        })
+    })
+
+function printerVisitas(){
+        var nis_para_print = []
+
+        $('input[name="excluir[]"]:checked').each(function (){
+            nis_para_print.push($(this).val())
+        })
+        if (nis_para_print.length > 0) {
+            $.ajax({
+                url:'/Suas-Tech/cadunico/controller/parecer/print_visit.php',
+                method: 'POST',
+                data: {nis_para_print: nis_para_print},
+                dataType: 'json',
+                success: function(response){
+                    if(response.printer){
+                        $('#paraPrint').text(response.corpo);
+                    } else {
+                        console.error('Erro no servidor: ' + response.mensagem);
+                    }
+                },
+                error: function (error) {
+                    console.error('Erro ao enviar dados: ' + error);
+                }
+            })
+        } else {
+        alert('Nenhum NIS selecionado para impressão.');
+        }
+}
