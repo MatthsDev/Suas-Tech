8c7ad34c53ed0fcb158924b128d37f7bdaea3063
emmanuelcadete <engmecanicocadete@gmail.com>
2024-03-09 01:00:00 -0300

    lindo

--- concessao/controller/print_conc.php
+++ concessao/controller/print_conc.php
@@ -1,6 +1,5 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/config/conexao.php';
-//include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/cadunico/controller/acesso_user/dados_usuario.php';
 
 header('Content-Type: application/json'); // Define o tipo de conteúdo como JSON
 
